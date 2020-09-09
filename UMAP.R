5f3cde8ea86311c10887ac6ec91de9d9e96a6a76
nshedd <58157630+nshedd@users.noreply.github.com>
2020-09-09 14:36:26 -0400

    Add files via upload

---
+++ UMAP.R
@@ -0,0 +1,11 @@
+df = readRDS("df.rds")
+library(umap)
+library(ggplot2)
+
+plotumap <- function(data) {
+  data_umap = umap(data)
+  data_layout = data.frame(data_umap$layout)
+  ggplot(data=data_layout, aes(x=X1, y=X2,)) +geom_point()
+}
+
+plotumap(df)
