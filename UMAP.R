83d7832b269dc957f1601a6af06ab00457cc4ca6
nshedd <58157630+nshedd@users.noreply.github.com>
2020-09-10 20:01:32 -0400

    Update UMAP.R

--- UMAP.R
+++ UMAP.R
@@ -5,7 +5,10 @@ library(ggplot2)
 plotumap <- function(data) {
   data_umap = umap(data)
   data_layout = data.frame(data_umap$layout)
-  ggplot(data=data_layout, aes(x=X1, y=X2,)) +geom_point()
+    
+  png("UMAPplot.png")
+  ggplot(data=data_layout, aes(x=X1, y=X2, color=types[,2])) +geom_point()
+  dev.off
 }
 
 plotumap(df)
