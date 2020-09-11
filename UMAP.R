30529aab95cbba36ab111db060d5cb35da15612c
nshedd <58157630+nshedd@users.noreply.github.com>
2020-09-11 15:09:03 -0400

    Update UMAP.R

--- UMAP.R
+++ UMAP.R
@@ -1,11 +1,18 @@
-df = readRDS("df.rds")
+
+df = readRDS("/data/projects/psychencode/df.rds")
+types = read.table("cell_types.tsv", sep ="\t", header=FALSE)
+ 
 library(umap)
 library(ggplot2)
 
-plotumap <- function(data) {
+dl = types[,2]
+
+plotumap <- function(data, labels) {
   data_umap = umap(data)
   data_layout = data.frame(data_umap$layout)
-  ggplot(data=data_layout, aes(x=X1, y=X2,)) +geom_point()
+  
+  ggplot(data=data_layout, aes(x=X1, y=X2, color=dl)) +geom_point()
+  ggsave("UMAPplot.png", device=png())
 }
 
-plotumap(df)
+plotumap(df, types)
