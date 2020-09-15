3fbdad287eb4873327f5b8a44ad6b45d5c736acd
nshedd <58157630+nshedd@users.noreply.github.com>
2020-09-15 13:24:01 -0400

    Update UMAP.R

--- UMAP.R
+++ UMAP.R
@@ -13,11 +13,11 @@ tdl = ttypes[2,]
 tdf = t(df)
 
 plotumap <- function(data, labels) {
-  data_umap = umap(data, min_dist=0.0001, spread=1)
+  data_umap = umap(data, min_dist=0.00001, spread=1)
   data_layout = data.frame(data_umap$layout)
   
   ggplot(data=data_layout, aes(x=X1, y=X2, color=labels)) +geom_point()
-  ggsave("UMAPplot_mindist001.png", device=)
+  ggsave("UMAPplot_mindist00001.png", device=)
 }
 
 plotumap(tdf, tdl)
