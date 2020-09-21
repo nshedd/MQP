59b69146428c434b9c3dfa78c773da064a52e28a
nshedd <58157630+nshedd@users.noreply.github.com>
2020-09-21 10:52:56 -0400

    Update Immune_UMAP.R

--- Immune_UMAP.R
+++ Immune_UMAP.R
@@ -6,12 +6,12 @@ library(ggplot2)
 
 tdf = t(df)
 
-plotumap <- function(data, labels) {
-  data_umap = umap(data, min_dist=0.02, n_neighbors=3)
+plotumap <- function(data) {
+  data_umap = umap(data, min_dist=0.02, n_neighbors=5)
   data_layout = data.frame(data_umap$layout)
   
-  ggplot(data=data_layout, aes(x=X1, y=X2, color=labels)) +geom_point()
+  ggplot(data=data_layout, aes(x=X1, y=X2)) +geom_point()
   ggsave("UMAPplot.png", device=)
 }
 
-plotumap(tdf, tdl)
+plotumap(tdf)
