c6175117bf7bba1488fb74bc684eba174fe204b2
nshedd <58157630+nshedd@users.noreply.github.com>
2020-09-29 11:16:38 -0400

    Update UMAP2.py

--- UMAP2.py
+++ UMAP2.py
@@ -52,7 +52,7 @@ def color_graph(matrix, cells, elements, marker):
         if marker_sum > 20:
             sums.append(marker_sum)
     matplotlib.pyplot.hist(sums, bins=50)
-    matplotlib.pyplot.savefig(os.path.expanduser("~/marker_colors4.svg"))
+    matplotlib.pyplot.savefig(os.path.expanduser("~/marker_colorsb1.svg"))
     matplotlib.pyplot.close()
     print("done")
 
@@ -60,9 +60,12 @@ def color_graph(matrix, cells, elements, marker):
 def main():
     elements, cells, matrix = read_matrix() # reads the matrix from the file
     #tmatrix = np.transpose(matrix)
-    t_type_link = "/data/zusers/pratth/ATAC/specific-elements/top-10k/unstimulated_T-cells.bed"
-    t_types = read_cell_types(t_type_link) # reads a cell type matrix
-    color_graph(matrix, cells, elements, t_types)
+    #t_type_link = "/data/zusers/pratth/ATAC/specific-elements/top-10k/unstimulated_T-cells.bed"
+    #t_types = read_cell_types(t_type_link) # reads a cell type matrix
+    #color_graph(matrix, cells, elements, t_types) ## later add colors to the input and just alter the color at an index if it is in a threshold
+    b_type_link = "/data/zusers/pratth/ATAC/specific-elements/top-10k/B-cell.bed"
+    b_types = read_cell_types(b_type_link) # reads a cell type matrix
+    color_graph(matrix, cells, elements, b_types)
     u = umap.UMAP(n_neighbors = 10, min_dist = 0.1, metric = 'euclidean') # initialize UMAP. different parameters might give better separation
     coordinates = u.fit_transform(matrix) # perform the transformation. outputs a list of 2D coordinates, one for each row
     #colors = match_types(elements, t_types)
@@ -71,7 +74,7 @@ def main():
         [ y for x, y in coordinates ], # extract the y-coordinates from the UMAP output
         marker = '.', # make the points small so the plot isn't too crowded
         alpha = 0.1, # make the points semi-transparent so it is easier to tell where points densely cluster together
-        c = colors # this makes unstimulated t cells blue and everything else black. TODO: replace with coloring by marker elements
+        #c = colors # this makes unstimulated t cells blue and everything else black. TODO: replace with coloring by marker elements
     )
     matplotlib.pyplot.savefig(os.path.expanduser("~/umap_colored.svg")) # write the plot to "umap.svg" in your home directory
     return 0
