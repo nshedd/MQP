0362c1467305445a68f231f250dd4be0d9ccba4b
nshedd <58157630+nshedd@users.noreply.github.com>
2020-09-29 15:58:51 -0400

    Update UMAP2.py

--- UMAP2.py
+++ UMAP2.py
@@ -38,7 +38,7 @@ def match_types(elements, types):
             c.append("black")
     return c
 
-def color_graph(matrix, cells, elements, marker):
+def color_graph(matrix, cells, elements, marker, colors):
     sums=[]
     indices = []
     for j in elements:
@@ -49,28 +49,44 @@ def color_graph(matrix, cells, elements, marker):
         for j in indices:
             if elements[j] in marker:
                 marker_sum = marker_sum + matrix[i][j]
-        if marker_sum > 50:
-            sums.append(marker_sum)
-    matplotlib.pyplot.hist(sums, bins=50)
-    matplotlib.pyplot.savefig(os.path.expanduser("~/marker_colorsm1.svg"))
-    matplotlib.pyplot.close()
-    print("done")
+        if marker == "t_types":
+            if marker_sum > 200:
+                colors[i]="red"
+        if marker == "b_types":
+            if marker_sum > 300:
+                if colors[i] == "red":
+                    colors[i]="purple"
+                else:
+                    colors[i]="blue"
+        if marker == "m_types":
+            if marker_sum > 350:
+                if colors[i] == "red"
+                    colors[i]="orange"
+                elif colors[i] == "blue":
+                    colors[i]="green"
+                elif colors[i] == "purple":
+                    colors[i]="brown"
+                else:
+                    colors[i]="yellow"
+    return colors
 
 
 def main():
     elements, cells, matrix = read_matrix() # reads the matrix from the file
 
-    #t_type_link = "/data/zusers/pratth/ATAC/specific-elements/top-10k/unstimulated_T-cells.bed"
-    #t_types = read_cell_types(t_type_link) # reads a cell type matrix
-    #color_graph(matrix, cells, elements, t_types) ## later add color_list to the input and just alter the color at an index if it is in a threshold
+    colors = ["black"] * len(cells)
 
-    #b_type_link = "/data/zusers/pratth/ATAC/specific-elements/top-10k/B-cell.bed"
-    #b_types = read_cell_types(b_type_link) # reads a cell type matrix
-    #color_graph(matrix, cells, elements, b_types)
+    t_type_link = "/data/zusers/pratth/ATAC/specific-elements/top-10k/unstimulated_T-cells.bed"
+    t_types = read_cell_types(t_type_link) # reads a cell type matrix
+    colors = color_graph(matrix, cells, elements, t_types, colors) ## later add color_list to the input and just alter the color at an index if it is in a threshold
+
+    b_type_link = "/data/zusers/pratth/ATAC/specific-elements/top-10k/B-cell.bed"
+    b_types = read_cell_types(b_type_link) # reads a cell type matrix
+    colors = color_graph(matrix, cells, elements, b_types, colors)
 
     m_type_link = "/data/zusers/pratth/ATAC/specific-elements/top-10k/myeloid_cells.bed"
     m_types = read_cell_types(m_type_link) # reads a cell type matrix
-    color_graph(matrix, cells, elements, m_types)
+    colors = color_graph(matrix, cells, elements, m_types, colors)
 
     u = umap.UMAP(n_neighbors = 10, min_dist = 0.1, metric = 'euclidean') # initialize UMAP. different parameters might give better separation
     coordinates = u.fit_transform(matrix) # perform the transformation. outputs a list of 2D coordinates, one for each row
@@ -80,7 +96,7 @@ def main():
         [ y for x, y in coordinates ], # extract the y-coordinates from the UMAP output
         marker = '.', # make the points small so the plot isn't too crowded
         alpha = 0.1, # make the points semi-transparent so it is easier to tell where points densely cluster together
-        #c = colors # this makes unstimulated t cells blue and everything else black. TODO: replace with coloring by marker elements
+        c = colors # this makes unstimulated t cells blue and everything else black. TODO: replace with coloring by marker elements
     )
     matplotlib.pyplot.savefig(os.path.expanduser("~/umap_colored.svg")) # write the plot to "umap.svg" in your home directory
     return 0
