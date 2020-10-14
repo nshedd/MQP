fe7d221ad8cfc5af61cc241e7576c63a12426291
nshedd <58157630+nshedd@users.noreply.github.com>
2020-10-14 05:43:47 -0400

    Update Immune_UMAP_set2.py

--- Immune_UMAP_set2.py
+++ Immune_UMAP_set2.py
@@ -62,8 +62,7 @@ def create_histograms(matrix, cells, elements, marker, marker_name):
         for j in indices:
             if elements[j] in marker:
                 marker_sum = marker_sum + matrix[i][j]
-        if marker_sum > 0.1:
-            sums.append(marker_sum)	
+        sums.append(marker_sum)	
     if marker_name=="t_types":
         matplotlib.pyplot.hist(sums, bins=50)
         matplotlib.pyplot.xlabel('Sum of Expression Across Top 10k Enhancer Elements')  
