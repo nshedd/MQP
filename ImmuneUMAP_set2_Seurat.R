c3158c322c8821c856d36690fa308e8ca4b51a1f
nshedd <58157630+nshedd@users.noreply.github.com>
2020-10-30 11:55:19 -0400

    Update ImmuneUMAP_set2_Seurat.R
    Added print functions and fixed name of seurat object

--- ImmuneUMAP_set2_Seurat.R
+++ ImmuneUMAP_set2_Seurat.R
@@ -4,20 +4,23 @@ library(patchwork)
 
 
 matrix = read.table("/data/zusers/pratth/sc/atac/GSM3722075_PBMC_Rep3_fragments.tsv.gz.rDHS.matrix.tsv", header=TRUE, sep="\t", row.names=1)
+print("loaded matrix")
 
-
-colors <- scan(path.expand("~/set2_top10k_colors.txt"), what=character, sep='\n', )
+colors <- scan(path.expand("~/set2_top10k_colors.txt"), what=character, sep='\n')
+print("loaded colors")
 
 set2umap <- CreateSeuratObject(counts = matrix, project = "set2", min.cells = 3, min.features = 200)
+print("created seurat object")
 
-matrix <- NormalizeData(matrix, normalization.method = "LogNormalize", scale.factor = 10000)
+set2umap <- NormalizeData(set2umap, normalization.method = "LogNormalize", scale.factor = 10000)
+print("normalized data")
 
-all.genes <- rownames(matrix)
-matrix <- ScaleData(matrix, features = all.genes)
+all.cells <- rownames(set2umap)
+set2umap <- ScaleData(set2umap, features = all.cells)
 
-matrix <- RunUMAP(matrix, dims = 1:10)
+set2umap <- RunUMAP(set2umap, dims = 1:10)
 
-DimPlot(matrix, reduction = "umap", do.return=TRUE)
+DimPlot(set2umap, reduction = "umap", do.return=TRUE)
 ggsave(path.expand("~/umap_colored_set2_seurat_default.svg"), device=)
 
 
