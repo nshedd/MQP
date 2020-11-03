b3f4cf2c56236c913b9211fe15b8ecfa7565a28b
nshedd <58157630+nshedd@users.noreply.github.com>
2020-11-03 13:43:14 -0500

    Update ImmuneUMAP_set2_Seurat.R
    added find neighbors and find clusters

--- ImmuneUMAP_set2_Seurat.R
+++ ImmuneUMAP_set2_Seurat.R
@@ -6,21 +6,23 @@ print("loaded packages")
 
 matrix = readRDS(file=path.expand("~/GSM3722075_PBMC_Rep3_fragments.rds"))
 print("loaded matrix")
+head(matrix)
 
 colors <- scan(path.expand("~/set2_top10k_colors.txt"), what=character(), sep='\n')
 print("loaded colors")
 
 set2umap <- CreateSeuratObject(counts = matrix, project = "set2", min.cells = 3, min.features = 200)
-print("created seurat object")
 
 set2umap <- NormalizeData(set2umap, normalization.method = "LogNormalize", scale.factor = 10000)
-print("normalized data")
 
 all_cells <- rownames(set2umap)
 set2umap <- ScaleData(set2umap, features = all_cells)
 
 set2umap <- FindVariableFeatures(object = set2umap)
 set2umap <- RunPCA(set2umap, features = VariableFeatures(object = set2umap))
+
+set2umap <- FindNeighbors(set2umap, dims = 1:10)
+set2umap <- FindClusters(set2umap, resolution = 0.5)
 
 set2umap <- RunUMAP(set2umap, dims = 1:10)
 
