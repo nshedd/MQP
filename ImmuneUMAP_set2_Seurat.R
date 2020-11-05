449abf6909ad377406a106af9fd7e327e29550cd
nshedd <58157630+nshedd@users.noreply.github.com>
2020-11-05 11:52:10 -0500

    Update ImmuneUMAP_set2_Seurat.R

--- ImmuneUMAP_set2_Seurat.R
+++ ImmuneUMAP_set2_Seurat.R
@@ -8,6 +8,10 @@ print("loaded packages")
 matrix = readRDS(file=path.expand("~/GSM3722075_PBMC_Rep3_fragments.rds"))
 print("loaded matrix")
 print(matrix[1:10, 1:10, drop=FALSE])
+print("Number of rows:")
+print(nrow(matrix))
+print("Number of columns:")
+print(ncol(matrix))
 
 colors <- scan(path.expand("~/set2_top10k_colors.txt"), what=character(), sep='\n')
 print("loaded colors")
@@ -31,4 +35,6 @@ plot = DimPlot(set2umap, reduction = "umap")
 ggsave(path.expand("~/umap_colored_set2_seurat_default.png"), device=)
 
 set2umap.markers <- FindAllMarkers(set2umap, only.pos = TRUE, min.pct = 0.25, thresh.use = 0.25)
-set2umap.markers %>% group_by(cluster) %>% top_n(5, avg_logFC)
+diff_expressed = set2umap.markers %>% group_by(cluster) %>% top_n(5, avg_logFC)
+
+write_tsv(diff_expressed, path.expand("~/set2_differentiallyexpressed.txt"))
