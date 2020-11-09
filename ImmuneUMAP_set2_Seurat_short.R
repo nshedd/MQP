ee61302f77a72e264eaaf8c1891b2c2e25c8422f
nshedd <58157630+nshedd@users.noreply.github.com>
2020-11-08 19:48:58 -0500

    Update ImmuneUMAP_set2_Seurat_short.R

--- ImmuneUMAP_set2_Seurat_short.R
+++ ImmuneUMAP_set2_Seurat_short.R
@@ -7,7 +7,7 @@ set2umap = readRDS(file=path.expand("~/GSM3722075_PBMC_Rep3_fragments_Seurat.rds
 
 T_cells = read.table("/data/zusers/pratth/ATAC/specific-elements/top-10k/unstimulated_T-cells.bed",header = FALSE, sep="\t",stringsAsFactors=FALSE, quote="")
 B_cells = read.table("/data/zusers/pratth/ATAC/specific-elements/top-10k/B-cell.bed",header = FALSE, sep="\t",stringsAsFactors=FALSE, quote="")
-M_cells = read.table("/data/zusers/pratth/ATAC/specific-elements/top-10k/myeloid_cells.bed.bed",header = FALSE, sep="\t",stringsAsFactors=FALSE, quote="")
+M_cells = read.table("/data/zusers/pratth/ATAC/specific-elements/top-10k/myeloid_cells.bed",header = FALSE, sep="\t",stringsAsFactors=FALSE, quote="")
 print("read tables")
 
 set2umap.markers <- FindAllMarkers(set2umap, only.pos = TRUE, min.pct = 0.25, thresh.use = 0.25)
