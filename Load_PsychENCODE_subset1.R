a41c77499075a0cfff9efa462747420bd71f1c1d
nshedd <58157630+nshedd@users.noreply.github.com>
2020-11-16 12:02:48 -0500

    Add files via upload

---
+++ Load_PsychENCODE_subset1.R
@@ -0,0 +1,7 @@
+matrix = read.table("~/subset.tsv", header=TRUE, row.names=1, sep='\t')
+
+print("finsihed loading data")
+
+saveRDS(matrix, file=path.expand("~/GRCh38-rDHSs_GW17_Cortex_50k_fragments_subset1.rds"))
+
+print("finished saving RDS")
