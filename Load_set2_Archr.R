7ee81468c7531341bd8b753b619c208d1a5c8cec
nshedd <58157630+nshedd@users.noreply.github.com>
2020-11-19 12:26:23 -0500

    add Seurat, see if compatible

--- Load_set2_Archr.R
+++ Load_set2_Archr.R
@@ -1,4 +1,7 @@
 library(ArchR)
+library(dplyr)
+library(Seurat)
+library(patchwork)
 
 sample_names = scan(what = path.expand("~/sample-names.txt"), sep = "\t")
 print(sample_names)
@@ -15,3 +18,8 @@ ArrowFiles <- createArrowFiles(
 )
 
 print("Done loading Archr file")
+
+ArrowFiles
+
+psychENCODE_whole <- CreateSeuratObject(counts = ArrowFiles, project = "psychENCODE_whole", min.cells = 3, min.features = 200)
+psychENCODE_whole
