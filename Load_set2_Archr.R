f7a595a727e39752473b7804609af85e3f235f79
nshedd <58157630+nshedd@users.noreply.github.com>
2020-11-19 12:05:29 -0500

    remove parameters

--- Load_set2_Archr.R
+++ Load_set2_Archr.R
@@ -6,10 +6,10 @@ print(sample_names)
 ArrowFiles <- createArrowFiles(
   inputFiles = "/data/zusers/pratth/sc/atac/GSM3722075_PBMC_Rep3_fragments.tsv.gz.rDHS.matrix.tsv",
   sampleNames = sample_names,
-  filterTSS = 4, #Dont set this too high because you can always increase later
-  filterFrags = 1000, 
-  addTileMat = TRUE,
-  addGeneScoreMat = TRUE
+  #filterTSS = 4, #Dont set this too high because you can always increase later
+  #filterFrags = 1000, 
+  #addTileMat = TRUE,
+  #addGeneScoreMat = TRUE
 )
 
 print("Done loading Archr file")
