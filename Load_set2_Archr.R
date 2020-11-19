6b389d52cb0a325a2006f6ca6fa67d27b38a95d7
nshedd <58157630+nshedd@users.noreply.github.com>
2020-11-19 11:59:56 -0500

    Add scan function to read sample names

--- Load_set2_Archr.R
+++ Load_set2_Archr.R
@@ -1,8 +1,10 @@
 library(ArchR)
 
+sample_names = scan(what = path.expand("~/sample-names.txt"), sep = "\t")
+
 ArrowFiles <- createArrowFiles(
   inputFiles = "/data/zusers/pratth/sc/atac/GSM3722075_PBMC_Rep3_fragments.tsv.gz.rDHS.matrix.tsv",
-  sampleNames = names(inputFiles),
+  sampleNames = sample_names,
   filterTSS = 4, #Dont set this too high because you can always increase later
   filterFrags = 1000, 
   addTileMat = TRUE,
