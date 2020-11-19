4664e1b512798252716ff5a18a5c810f8329ec82
nshedd <58157630+nshedd@users.noreply.github.com>
2020-11-19 12:27:32 -0500

    edit parameters troubleshoot

--- Load_set2_Archr.R
+++ Load_set2_Archr.R
@@ -11,8 +11,8 @@ addArchRGenome("hg19")
 ArrowFiles <- createArrowFiles(
   inputFiles = "/data/zusers/pratth/sc/atac/GSM3722075_PBMC_Rep3_fragments.tsv.gz.rDHS.matrix.tsv",
   sampleNames = sample_names,
-  filterTSS = 4, #Dont set this too high because you can always increase later
-  filterFrags = 1000, 
+  #filterTSS = 4, #Dont set this too high because you can always increase later
+  #filterFrags = 1000, 
   addTileMat = TRUE,
   addGeneScoreMat = TRUE
 )
