817e0491bc8e69aabb9d93d82d893efbdba2f3eb
nshedd <58157630+nshedd@users.noreply.github.com>
2020-11-19 11:40:08 -0500

    Removed installation commands

--- Load_set2_Archr.R
+++ Load_set2_Archr.R
@@ -1,8 +1,4 @@
-if (!requireNamespace("devtools", quietly = TRUE)) install.packages("devtools")
-if (!requireNamespace("BiocManager", quietly = TRUE)) install.packages("BiocManager")
-devtools::install_github("GreenleafLab/ArchR", ref="master", repos = BiocManager::repositories())
 library(ArchR)
-ArchR::installExtraPackages()
 
 ArrowFiles <- createArrowFiles(
   inputFiles = "/data/zusers/pratth/sc/atac/GSM3722075_PBMC_Rep3_fragments.tsv.gz.rDHS.matrix.tsv",
