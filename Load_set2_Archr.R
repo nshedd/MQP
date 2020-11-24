ad8aa5975aa99865be81458770f11a6b08f04396
nshedd <58157630+nshedd@users.noreply.github.com>
2020-11-24 14:26:30 -0500

    Update Load_set2_Archr.R

--- Load_set2_Archr.R
+++ Load_set2_Archr.R
@@ -1,18 +1,30 @@
 library(ArchR)
 
-#GRCh38
 addArchRGenome("hg38")
 
 args <- commandArgs(trailingOnly = TRUE)
 time = args[1]
-key = c("Temporal_GW20")
-bam = c("/data/zusers/pratth/sc/PEC/Temporal_GW20.sorted.tsv.gz")
+bam = "Temporal_GW20.sorted.fragments.tsv.gz"
+key = "temporal"
 
 ArrowFiles = createArrowFiles( inputFiles = bam, sampleNames = key,
-                               filterTSS = 4, filterFrags = 1000, addTileMat = TRUE, addGeneScoreMat = TRUE, 
-                               gsubExpression=":.*"
-                               )
-ArrowFiles
-
+  filterTSS = 4, filterFrags = 1000, addTileMat = TRUE, addGeneScoreMat = TRUE, 
+  gsubExpression=":.*")
 
 proj = ArchRProject(ArrowFiles = ArrowFiles, outputDirectory = time, copyArrows = TRUE)
+rdhss = import("/data/projects/encode/Registry/V2/GRCh38/GRCh38-rDHSs.bed")
+
+p=getEmbedding(ArchRProj = proj, embedding = "UMAP_Peak")
+t=getEmbedding(ArchRProj = proj, embedding = "UMAP_Tile")
+
+png(paste0("UMAP-Peak-",time,".png"), width=10, height=8, units="in", res=300)
+ggplot(p, aes(x=p[,1], y=p[,2], color=proj@cellColData$Clusters_Peak))+
+  geom_point()#+scale_color_manual(values=c("red4","limegreen","green4","red3","darkorchid","violetred2","royalblue"))
+dev.off()
+
+png(paste0("UMAP-Tile-",time,".png"), width=10, height=8, units="in", res=300)
+ggplot(t, aes(x=t[,1], y=t[,2], color=proj@cellColData$Clusters_Tile))+
+  geom_point()#+scale_color_manual(values=c("red4","limegreen","green4","darkorchid","violetred2","royalblue"))
+dev.off()
+
+saveArchRProject(ArchRProj = proj, outputDirectory = time, overwrite = TRUE, load = TRUE)
