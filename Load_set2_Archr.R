96f26025f4aa9478c6fb8e8b5201201f4fc331ea
nshedd <58157630+nshedd@users.noreply.github.com>
2020-11-24 14:27:41 -0500

    Update Load_set2_Archr.R

--- Load_set2_Archr.R
+++ Load_set2_Archr.R
@@ -4,7 +4,7 @@ addArchRGenome("hg38")
 
 args <- commandArgs(trailingOnly = TRUE)
 time = args[1]
-bam = "Temporal_GW20.sorted.fragments.tsv.gz"
+bam = "/data/zusers/pratth/sc/PEC/Temporal_GW20.sorted.fragments.tsv.gz"
 key = "temporal"
 
 ArrowFiles = createArrowFiles( inputFiles = bam, sampleNames = key,
