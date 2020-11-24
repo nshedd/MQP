387c8eb212fb93dff7033251efa8b2813aa12a5b
nshedd <58157630+nshedd@users.noreply.github.com>
2020-11-24 12:31:15 -0500

    Update Load_set2_Archr.R

--- Load_set2_Archr.R
+++ Load_set2_Archr.R
@@ -6,7 +6,7 @@ addArchRGenome("hg38")
 args <- commandArgs(trailingOnly = TRUE)
 time = args[1]
 key = c("Temporal_GW20")
-bam = c("/data/zusers/pratth/sc/PEC/Temporal_GW20.tsv.gz")
+bam = c("/data/zusers/pratth/sc/PEC/Temporal_GW20_sorted.tsv.gz")
 
 ArrowFiles = createArrowFiles( inputFiles = bam, sampleNames = key,
                                filterTSS = 4, filterFrags = 1000, addTileMat = TRUE, addGeneScoreMat = TRUE, 
