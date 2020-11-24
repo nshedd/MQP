2b9a91c86a73b7460ab57784ac12a04208025e65
nshedd <58157630+nshedd@users.noreply.github.com>
2020-11-24 11:42:14 -0500

    Update Load_set2_Archr.R

--- Load_set2_Archr.R
+++ Load_set2_Archr.R
@@ -10,7 +10,7 @@ bam = c("/data/zusers/pratth/sc/PEC/Temporal_GW20.tsv.gz")
 
 ArrowFiles = createArrowFiles( inputFiles = bam, sampleNames = key,
                                filterTSS = 4, filterFrags = 1000, addTileMat = TRUE, addGeneScoreMat = TRUE, 
-                               #gsubExpression=":.*"
+                               gsubExpression=":.*"
                                )
 ArrowFiles
 
