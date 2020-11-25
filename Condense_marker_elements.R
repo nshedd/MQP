3e4ab39aa200ce2a86f897786b52b5712e33f8a3
nshedd <58157630+nshedd@users.noreply.github.com>
2020-11-24 23:23:25 -0500

    Add files via upload

---
+++ Condense_marker_elements.R
@@ -0,0 +1,14 @@
+marker_elements = read.table(path.expand("~/temporal_marker_peaks.txt"), sep = '\t', header=TRUE)
+
+top_10_elements = marker_elements[1,]
+
+for (i in range(0:10)) {
+  temp_elements = marker_elements[marker_elements$group_name = i,]
+  temp_elements_sorted = order(temp_elements$Log2FC)
+  top_temp_elements = temp_elements_sorted[1:10,]
+  top_10_elements = rbind(top_10_elements, top_temp_elements)
+}
+  
+top_10_elements = top_10_elements[!1,]
+
+write.table(markerList, file = path.expand("~/temporal_marker_peaks_top10.txt"), sep = '\t')
