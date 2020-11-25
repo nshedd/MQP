d68436fad514e365737672308ba2e2fbda5c3ad5
nshedd <58157630+nshedd@users.noreply.github.com>
2020-11-24 23:49:27 -0500

    Update Condense_marker_elements.R

--- Condense_marker_elements.R
+++ Condense_marker_elements.R
@@ -7,7 +7,7 @@ for (i in 1:11) {
   temp_elements = marker_elements[marker_elements$group == i,]
   temp_elements_sorted = temp_elements[order(temp_elements$Log2FC),]
   j = nrow(temp_elements_sorted)
-  print(j)
+  head(temp_elements_sorted)
   if (j >= 10) {
     top_temp_elements = temp_elements_sorted[1:10,]
   }
