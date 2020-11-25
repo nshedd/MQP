f85be45ce11e95247bfd3369bc2d251045d8c38a
nshedd <58157630+nshedd@users.noreply.github.com>
2020-11-24 23:38:13 -0500

    Update Condense_marker_elements.R

--- Condense_marker_elements.R
+++ Condense_marker_elements.R
@@ -1,10 +1,10 @@
-marker_elements = read.table(path.expand("~/temporal_marker_peaks.txt"), sep = '\t', header=TRUE)
+marker_elements = read.table(path.expand("~/temporal_marker_peaks.txt"), sep = '\t', header=TRUE, row.names=1)
 head(marker_elements)
 
 top_10_elements = marker_elements[1,]
 
 for (i in 0:10) {
-  temp_elements = marker_elements[marker_elements$group_name == i,]
+  temp_elements = marker_elements[marker_elements$group == i,]
   print(temp_elements)
   temp_elements_sorted = order(temp_elements$Log2FC)
   j = nrow(temp_elements_sorted)
