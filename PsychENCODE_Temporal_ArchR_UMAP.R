498c6f54c7cbf86ff7ff86a3ee1784587d121863
nshedd <58157630+nshedd@users.noreply.github.com>
2020-11-24 19:10:06 -0500

    Update PsychENCODE_Temporal_ArchR_UMAP.R

--- PsychENCODE_Temporal_ArchR_UMAP.R
+++ PsychENCODE_Temporal_ArchR_UMAP.R
@@ -36,15 +36,12 @@ proj = addUMAP(ArchRProj = proj, reducedDims = "IterativeLSI_Peak", name = "UMAP
                nNeighbors = 10, minDist = 0.1, metric = "cosine", force=TRUE)
 
 
-p=getEmbedding(ArchRProj = proj, embedding = "UMAP_Peak")
-t=getEmbedding(ArchRProj = proj, embedding = "UMAP_Tile")
+p=plotEmbedding(ArchRProj = proj, embedding = "UMAP_Peak")
+t=plotEmbedding(ArchRProj = proj, embedding = "UMAP_Tile")
 
-png(paste0("UMAP-Peak-",time,".png"), width=10, height=8, units="in", res=300)
-ggplot(p, aes(x=p[,1], y=p[,2], color=proj@cellColData$Clusters_Peak))+
-  geom_point()#+scale_color_manual(values=c("red4","limegreen","green4","red3","darkorchid","violetred2","royalblue"))
-dev.off()
+ggAlignPlots(p, t, type = "h")
 
-png(paste0("UMAP-Tile-",time,".png"), width=10, height=8, units="in", res=300)
-ggplot(t, aes(x=t[,1], y=t[,2], color=proj@cellColData$Clusters_Tile))+
-  geom_point()#+scale_color_manual(values=c("red4","limegreen","green4","darkorchid","violetred2","royalblue"))
-dev.off()
+plotPDF(p,t, name = "Plot-UMAP-Sample-Clusters-2.pdf",
+        ArchRProj = proj, addDOC = FALSE, width = 5, height = 5)
+
+saveArchRProject(ArchRProj = proj, outputDirectory = time, overwrite = TRUE, load = TRUE)
