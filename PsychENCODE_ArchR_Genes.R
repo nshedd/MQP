e803818880ca2347cecf7c859bd50699d29166f7
nshedd <58157630+nshedd@users.noreply.github.com>
2020-11-30 16:54:24 -0500

    Add files via upload

---
+++ PsychENCODE_ArchR_Genes.R
@@ -0,0 +1,33 @@
+library(ArchR)
+
+time = "Temporal_Dev_Analysis"
+
+proj <- loadArchRProject(path = time)
+
+proj <- addImputeWeights(proj)
+
+p <- plotEmbedding(
+  ArchRProj = projHeme2, 
+  colorBy = "GeneScoreMatrix",
+  embedding = "UMAP",
+  imputeWeights = getImputeWeights(projHeme2)
+)
+
+p2 <- lapply(p, function(x){
+  x + guides(color = FALSE, fill = FALSE) + 
+    theme_ArchR(baseSize = 6.5) +
+    theme(plot.margin = unit(c(0, 0, 0, 0), "cm")) +
+    theme(
+      axis.text.x=element_blank(), 
+      axis.ticks.x=element_blank(), 
+      axis.text.y=element_blank(), 
+      axis.ticks.y=element_blank()
+    )
+})
+do.call(cowplot::plot_grid, c(list(ncol = 3),p2))
+
+
+plotPDF(plotList = p, 
+        name = "Plot-UMAP-Temporal-Marker-Genes.pdf", 
+        ArchRProj = projHeme2, 
+        addDOC = FALSE, width = 5, height = 5)
