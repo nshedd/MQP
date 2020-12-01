84c7520463f7132bf114b9ce8d53369009ae55b3
nshedd <58157630+nshedd@users.noreply.github.com>
2020-12-01 16:55:12 -0500

    Update PsychENCODE_ArchR_UMAP_PFC.R

--- PsychENCODE_ArchR_UMAP_PFC.R
+++ PsychENCODE_ArchR_UMAP_PFC.R
@@ -12,6 +12,8 @@ ArrowFiles = createArrowFiles( inputFiles = bam, sampleNames = key,
   filterTSS = 4, filterFrags = 1000, addTileMat = TRUE, addGeneScoreMat = TRUE, 
   gsubExpression=":.*")
 
+print("made it here")
+
 proj = ArchRProject(ArrowFiles = ArrowFiles, outputDirectory = time, copyArrows = TRUE)
 rdhss = import("/data/projects/encode/Registry/V2/GRCh38/GRCh38-rDHSs.bed")
 
