eb5a16d5676b2b8a442ed7f770eb5b98c3dbbcba
nshedd <58157630+nshedd@users.noreply.github.com>
2020-12-01 10:42:22 -0500

    Update PsychENCODE_ArchR_Motifs.R

--- PsychENCODE_ArchR_Motifs.R
+++ PsychENCODE_ArchR_Motifs.R
@@ -4,43 +4,16 @@ time = "Temporal_Dev_Analysis"
 
 proj <- loadArchRProject(path = time)
 
-markerTest <- getMarkerFeatures(
-  ArchRProj = proj, 
-  useMatrix = "PeakMatrix",
-  groupBy = "Clusters2",
-  testMethod = "wilcoxon",
-  bias = c("TSSEnrichment", "log10(nFrags)"),
-  useGroups = "C1",
-  bgdGroups = "C2"
-)
-
-#proj <- addMotifAnnotations(ArchRProj = proj, motifSet = "cisbp", name = "Motif")
+#if("Motif" %ni% names(proj@peakAnnotation)){
+#  proj <- addMotifAnnotations(ArchRProj = proj, motifSet = "cisbp", name = "Motif")
+#}
 
 #saveArchRProject(ArchRProj = proj)
 
-motifsUp <- peakAnnoEnrichment(
-    seMarker = markerTest,
-    ArchRProj = proj,
-    peakAnnotation = "Motif",
-    cutOff = "FDR <= 0.1 & Log2FC >= 0.5"
-  )
+proj <-addBgdPeaks(proj, method="ArchR")
 
-df <- data.frame(TF = rownames(motifsUp), mlog10Padj = assay(motifsUp)[,1])
-df <- df[order(df$mlog10Padj, decreasing = TRUE),]
-df$rank <- seq_len(nrow(df))
+proj <- addDeviationsMatrix(ArchRProj = proj, peakAnnotation = "Motif", force = TRUE)
 
-ggUp <- ggplot(df, aes(rank, mlog10Padj, color = mlog10Padj)) + 
-  geom_point(size = 1) +
-  ggrepel::geom_label_repel(
-        data = df[rev(seq_len(30)), ], aes(x = rank, y = mlog10Padj, label = TF), 
-        size = 1.5,
-        nudge_x = 2,
-        color = "black"
-  ) + theme_ArchR() + 
-  ylab("-log10(P-adj) Motif Enrichment") + 
-  xlab("Rank Sorted TFs Enriched") +
-  scale_color_gradientn(colors = paletteContinuous(set = "comet"))
+plotVarDev <- getVarDeviations(proj, name = "MotifMatrix", plot = TRUE)
 
-plotPDF(ggUp, name = "Motif-Enrichment", width = 5, height = 5, ArchRProj = proj, addDOC = FALSE)
-
-saveArchRProject(ArchRProj = proj)
+plotPDF(plotVarDev, name = "Variable-Motif-Deviation-Scores", width = 5, height = 5, ArchRProj = proj, addDOC = FALSE)
