de4910e78b7fb032a703508c6eb235b1f759adfb
nshedd <58157630+nshedd@users.noreply.github.com>
2020-12-04 12:33:16 -0500

    Update PsychENCODE_ArchR_Genes.R

--- PsychENCODE_ArchR_Genes.R
+++ PsychENCODE_ArchR_Genes.R
@@ -12,7 +12,11 @@ markersGS <- getMarkerFeatures(
     testMethod = "wilcoxon"
 )
 
+write.table(markerList, file = path.expand("~/temporal_marker_gene_score.txt"), sep = '\t')
+
 markerList <- getMarkers(markersGS, cutOff = "FDR <= 0.01 & Log2FC >= 1.25")
+
+write.table(markerList, file = path.expand("~/temporal_marker_genes.txt"), sep = '\t')
 
 heatmapGS <- markerHeatmap(
   seMarker = markersGS, 
@@ -23,3 +27,5 @@ heatmapGS <- markerHeatmap(
 ComplexHeatmap::draw(heatmapGS, heatmap_legend_side = "bot", annotation_legend_side = "bot")
 
 plotPDF(heatmapGS, name = "GeneScores-Marker-Heatmap", width = 8, height = 6, ArchRProj = proj, addDOC = FALSE)
+
+saveArchRProject(ArchRProj = proj)
