47bd5694dd250b35f14b2f0193555508b5f36c36
nshedd <58157630+nshedd@users.noreply.github.com>
2020-12-04 15:24:22 -0500

    use encode instead of cisbp

--- PsychENCODE_ArchR_Motifs.R
+++ PsychENCODE_ArchR_Motifs.R
@@ -7,11 +7,22 @@ time = "Temporal_Dev_Analysis"
 proj <- loadArchRProject(path = time)
 
 if("Motif" %ni% names(proj@peakAnnotation)){
-  proj <- addMotifAnnotations(ArchRProj = proj, motifSet = "cisbp", name = "Motif")
+  proj <- addMotifAnnotations(ArchRProj = proj, motifSet = "encode", name = "Motif")
 }
 
 markersPeaks <- getMarkerFeatures(ArchRProj = proj, useMatrix = "PeakMatrix", groupBy = "Clusters",
                                   bias = c("TSSEnrichment", "log10(nFrags)"),testMethod = "wilcoxon")
+
+markerList <- getMarkers(markersPeaks, cutOff = "FDR <= 0.01 & Log2FC >= 1")
+write.table(markerList, file = path.expand("~/temporal_marker_peaks_encode.txt"), sep = '\t')
+
+heatmapPeaks <- markerHeatmap(
+  seMarker = markersPeaks, 
+  cutOff = "FDR <= 0.1 & Log2FC >= 0.5",
+  transpose = TRUE
+)
+draw(heatmapPeaks, heatmap_legend_side = "bot", annotation_legend_side = "bot")
+plotPDF(heatmapPeaks, name = "Peak-Marker-Heatmap-Temporal-ENCODE", width = 8, height = 6, ArchRProj = proj, addDOC = FALSE)
 
 enrichMotifs <- peakAnnoEnrichment(
   seMarker = markersPeaks,
@@ -24,6 +35,6 @@ heatmapEM <- plotEnrichHeatmap(enrichMotifs, n = 7, transpose = TRUE)
 
 ComplexHeatmap::draw(heatmapEM, heatmap_legend_side = "bot", annotation_legend_side = "bot")
 
-plotPDF(heatmapEM, name = "Motifs-Enriched-Marker-Heatmap", width = 8, height = 6, ArchRProj = proj, addDOC = FALSE)
+plotPDF(heatmapEM, name = "Motifs-Enriched-Marker-Heatmap_ENCODE", width = 8, height = 6, ArchRProj = proj, addDOC = FALSE)
 
 saveArchRProject(ArchRProj = proj)
