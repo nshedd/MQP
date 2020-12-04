d950987b75fbe5fdcdde27f082b093156a7b00d6
nshedd <58157630+nshedd@users.noreply.github.com>
2020-12-04 12:22:07 -0500

    Update PsychENCODE_ArchR_Genes.R

--- PsychENCODE_ArchR_Genes.R
+++ PsychENCODE_ArchR_Genes.R
@@ -4,16 +4,22 @@ time = "Temporal_Dev_Analysis"
 
 proj <- loadArchRProject(path = time)
 
-proj <- addCoAccessibility(
-  ArchRProj = proj,
-  reducedDims = "IterativeLSI"
+markersGS <- getMarkerFeatures(
+    ArchRProj = proj, 
+    useMatrix = "GeneScoreMatrix", 
+    groupBy = "Clusters",
+    bias = c("TSSEnrichment", "log10(nFrags)"),
+    testMethod = "wilcoxon"
 )
 
-cA <- getCoAccessibility(
-  ArchRProj = proj,
-  corCutOff = 0.5,
-  resolution = 1,
-  returnLoops = FALSE
+markerList <- getMarkers(markersGS, cutOff = "FDR <= 0.01 & Log2FC >= 1.25")
+
+heatmapGS <- markerHeatmap(
+  seMarker = markersGS, 
+  cutOff = "FDR <= 0.01 & Log2FC >= 1.25",
+  transpose = TRUE
 )
 
-metadata(cA)[[1]]
+ComplexHeatmap::draw(heatmapGS, heatmap_legend_side = "bot", annotation_legend_side = "bot")
+
+plotPDF(heatmapGS, name = "GeneScores-Marker-Heatmap", width = 8, height = 6, ArchRProj = proj, addDOC = FALSE)
