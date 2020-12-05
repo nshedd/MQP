e1a08ea44dba4febe91c1f3b19cdeef069007512
nshedd <58157630+nshedd@users.noreply.github.com>
2020-12-04 20:19:43 -0500

    Create PsychENCODE_Temporal_Genes_CellType_Label.R

---
+++ PsychENCODE_Temporal_Genes_CellType_Label.R
@@ -0,0 +1,16 @@
+
+diff_expressed = read.table(path.expand("~/temporal_marker_genes.txt"), header=TRUE, row.names=1)
+
+celltypes <- character()
+for (gene in diff_expressed$name) {
+  if (gene %in% brain_genes$Human_Gene) {
+    celltypes <- c(celltypes, T_cells$Human_Gene)
+  }
+  else {
+    celltypes <- c(celltypes, "unknown")
+  }
+}
+
+diff_expressed$cell_type <- celltypes
+
+write.table(diff_expressed, file = path.expand("~/temporal_marker_genes_celltypes.txt"), sep = '\t')
