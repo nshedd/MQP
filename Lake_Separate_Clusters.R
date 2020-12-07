2e7a848b175458f9133356d67edf185347711c5b
nshedd <58157630+nshedd@users.noreply.github.com>
2020-12-07 15:50:27 -0500

    Update Lake_Separate_Clusters.R

--- Lake_Separate_Clusters.R
+++ Lake_Separate_Clusters.R
@@ -1,4 +1,4 @@
-genes = read.table(path.expand("~/GSE97930_frontalcortex_differentiallyexpressed.txt"), header=TRUE, row.names=1, sep="\t")
+genes = read.table(path.expand("~/GSE97930_cerebellarhem_differentiallyexpressed.txt"), header=TRUE, row.names=1, sep="\t")
 
 cluster0_genes = genes$gene[genes$cluster == '0']
 cluster1_genes = genes$gene[genes$cluster == '1']
@@ -14,16 +14,16 @@ cluster10_genes = genes$gene[genes$cluster == '10']
 cluster11_genes = genes$gene[genes$cluster == '11']
 
 
-write.table(cluster0_genes, path.expand("~/GSE97930_frontalcortex_genes/cluster0.txt"), sep = "\n", row.names=F)
-write.table(cluster1_genes, path.expand("~/GSE97930_frontalcortex_genes/cluster1.txt"), sep = "\n", row.names=F)
-write.table(cluster2_genes, path.expand("~/GSE97930_frontalcortex_genes/cluster2.txt"), sep = "\n", row.names=F)
-write.table(cluster3_genes, path.expand("~/GSE97930_frontalcortex_genes/cluster3.txt"), sep = "\n", row.names=F)
-write.table(cluster4_genes, path.expand("~/GSE97930_frontalcortex_genes/cluster4.txt"), sep = "\n", row.names=F)
-write.table(cluster5_genes, path.expand("~/GSE97930_frontalcortex_genes/cluster5.txt"), sep = "\n", row.names=F)
-write.table(cluster6_genes, path.expand("~/GSE97930_frontalcortex_genes/cluster6.txt"), sep = "\n", row.names=F)
-write.table(cluster7_genes, path.expand("~/GSE97930_frontalcortex_genes/cluster7.txt"), sep = "\n", row.names=F)
-write.table(cluster8_genes, path.expand("~/GSE97930_frontalcortex_genes/cluster8.txt"), sep = "\n", row.names=F)
-write.table(cluster9_genes, path.expand("~/GSE97930_frontalcortex_genes/cluster9.txt"), sep = "\n", row.names=F)
-write.table(cluster10_genes, path.expand("~/GSE97930_frontalcortex_genes/cluster10.txt"), sep = "\n", row.names=F)
-write.table(cluster11_genes, path.expand("~/GSE97930_frontalcortex_genes/cluster11.txt"), sep = "\n", row.names=F)
+write.table(cluster0_genes, path.expand("~/GSE97930_cerebellarhem_genes/cluster0.txt"), sep = "\n", row.names=F)
+write.table(cluster1_genes, path.expand("~/GSE97930_cerebellarhem_genes/cluster1.txt"), sep = "\n", row.names=F)
+write.table(cluster2_genes, path.expand("~/GSE97930_cerebellarhem_genes/cluster2.txt"), sep = "\n", row.names=F)
+write.table(cluster3_genes, path.expand("~/GSE97930_cerebellarhem_genes/cluster3.txt"), sep = "\n", row.names=F)
+write.table(cluster4_genes, path.expand("~/GSE97930_cerebellarhem_genes/cluster4.txt"), sep = "\n", row.names=F)
+write.table(cluster5_genes, path.expand("~/GSE97930_cerebellarhem_genes/cluster5.txt"), sep = "\n", row.names=F)
+write.table(cluster6_genes, path.expand("~/GSE97930_cerebellarhem_genes/cluster6.txt"), sep = "\n", row.names=F)
+write.table(cluster7_genes, path.expand("~/GSE97930_cerebellarhem_genes/cluster7.txt"), sep = "\n", row.names=F)
+write.table(cluster8_genes, path.expand("~/GSE97930_cerebellarhem_genes/cluster8.txt"), sep = "\n", row.names=F)
+write.table(cluster9_genes, path.expand("~/GSE97930_cerebellarhem_genes/cluster9.txt"), sep = "\n", row.names=F)
+write.table(cluster10_genes, path.expand("~/GSE97930_cerebellarhem_genes/cluster10.txt"), sep = "\n", row.names=F)
+write.table(cluster11_genes, path.expand("~/GSE97930_cerebellarhem_genes/cluster11.txt"), sep = "\n", row.names=F)
 
