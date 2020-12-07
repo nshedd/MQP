d3f9cd4e5623c1b806e4d8f3cd45e531f99d6a4e
nshedd <58157630+nshedd@users.noreply.github.com>
2020-12-07 12:21:27 -0500

    Update snDrop-seq_pipeline.R

--- snDrop-seq_pipeline.R
+++ snDrop-seq_pipeline.R
@@ -24,13 +24,13 @@ cerebellarhem <- FindClusters(cerebellarhem, resolution = 0.5)
 
 cerebellarhem <- RunUMAP(cerebellarhem, dims = 1:10)
 
-saveRDS(set2umap, file = path.expand("~/GSE97930_CerebellarHem_snDrop-seq_UMI_Count_Matrix_Seurat.rds"))
+saveRDS(cerebellarhem, file = path.expand("~/GSE97930_CerebellarHem_snDrop-seq_UMI_Count_Matrix_Seurat.rds"))
 
-plot = DimPlot(set2umap, reduction = "umap")
+plot = DimPlot(cerebellarhem, reduction = "umap")
 ggsave(path.expand("~/umap_GSE97930_CerebellarHem_Seurat_default.png"), device=)
 
-set2umap.markers <- FindAllMarkers(set2umap, only.pos = TRUE, min.pct = 0.25, thresh.use = 0.25)
-diff_expressed = set2umap.markers %>% group_by(cluster)
+set2umap.markers <- FindAllMarkers(cerebellarhem, only.pos = TRUE, min.pct = 0.25, thresh.use = 0.25)
+diff_expressed = cerebellarhem.markers %>% group_by(cluster)
 
 path2 = path.expand("~/Zlab single-cell marker genes - Brain.tsv")
 
