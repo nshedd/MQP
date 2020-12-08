120c2fcff29ff11cc8af0ecb7ed813045ac74a55
nshedd <58157630+nshedd@users.noreply.github.com>
2020-12-08 14:39:02 -0500

    Update PsychENCODE_Temporal_Genes_CellType_Label.R

--- PsychENCODE_Temporal_Genes_CellType_Label.R
+++ PsychENCODE_Temporal_Genes_CellType_Label.R
@@ -1,12 +1,14 @@
 
-brain_genes = read.table("Zlab single-cell marker genes - Brain.tsv", header=TRUE, sep="\t")
+brain_genes = read.table("Zlab single-cell marker genes - Brain.tsv 2", header=TRUE, sep="\t")
 
 diff_expressed = read.table(path.expand("~/temporal_marker_genes.txt"), header=TRUE, row.names=1, sep="\t")
+
+print(
 
 celltypes <- character()
 for (gene in diff_expressed$name) {
   if (gene %in% brain_genes$Human_Gene) {
-    celltypes <- c(celltypes, T_cells$Human_Gene)
+    celltypes <- c(celltypes, brain_genes$Human_Gene)
   }
   else {
     celltypes <- c(celltypes, "unknown")
