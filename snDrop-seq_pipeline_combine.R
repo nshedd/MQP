c1ea2049481a97e9253f7253354110fecf5ab358
nshedd <58157630+nshedd@users.noreply.github.com>
2020-12-16 16:40:26 -0500

    Update snDrop-seq_pipeline_combine.R

--- snDrop-seq_pipeline_combine.R
+++ snDrop-seq_pipeline_combine.R
@@ -1,6 +1,10 @@
+matrixa = readRDS(path.expand("~/GSE97930_VisualCortex.RDS"))
+matrixb = readRDS(path.expand("~/GSE97930_FrontalCortex.RDS"))
 matrixc = readRDS(path.expand("~/GSE97930_CerebellarHem.RDS"))
 matrixab = readRDS(path.expand("~/GSE97930_All.RDS"))
 
-combinedmatrix = merge(matrixab, matrixc, by=0)
+combinedmatrix = merge(matrixa, matrixb, by=0)
+
+combinedmatrix[0:10,0:10]
 
 saveRDS(combinedmatrix, file = path.expand("~/GSE97930_All.RDS"))
