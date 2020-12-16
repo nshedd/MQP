4568d130104cc8237ee07851b6e4d3d939917450
nshedd <58157630+nshedd@users.noreply.github.com>
2020-12-16 17:54:12 -0500

    Update snDrop-seq_pipeline.R

--- snDrop-seq_pipeline.R
+++ snDrop-seq_pipeline.R
@@ -4,7 +4,7 @@ library(patchwork)
 library(ggplot2)
 
 
-path1 = path.expand("~/GSE97930_CerebellarHem_snDrop-seq_UMI_Count_Matrix_08-01-2017.txt.gz")
+path1 = path.expand("~/GSE97930_CerebellarHem_snDrop-seq_UMI_Count_Matrix_08-01-2017.txt")
 
 matrix = read.table(path1, header=TRUE, row.names=1)
 
