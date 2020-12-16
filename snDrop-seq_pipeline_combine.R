1d3f9451b11aa492fc62e63479581534a4619ef9
nshedd <58157630+nshedd@users.noreply.github.com>
2020-12-16 14:51:58 -0500

    Update snDrop-seq_pipeline_combine.R

--- snDrop-seq_pipeline_combine.R
+++ snDrop-seq_pipeline_combine.R
@@ -1,21 +1,7 @@
-path1a = path.expand("~/GSE97930_VisualCortex_snDrop-seq_UMI_Count_Matrix_08-01-2017.txt.gz")
-path1b = path.expand("~/GSE97930_FrontalCortex_snDrop-seq_UMI_Count_Matrix_08-01-2017.txt.gz")
-path1c = path.expand("~/GSE97930_CerebellarHem_snDrop-seq_UMI_Count_Matrix_08-01-2017.txt")
+matrixa = readRDS(path.expand("~/GSE97930_VisualCortex.RDS"))
+matrixb = readRDS(path.expand("~/GSE97930_FrontalCortex.RDS"))
+#matrixc = readRDS(path.expand("~/GSE97930_CerebellarHem.RDS"))
 
-matrixa = read.table(path1a, header=TRUE, row.names=1)
-matrixb = read.table(path1b, header=TRUE, row.names=1)
-matrixc = read.table(path1c, header=TRUE, row.names=1)
+combinedmatrix = merge(matrixa, matrixb)
 
-saveRDS(matrixa, path.expand("~/GSE97930_VisualCortex.RDS"))
-saveRDS(matrixb, path.expand("~/GSE97930_FrontalCortex.RDS"))
-saveRDS(matrixc, path.expand("~/GSE97930_CerebellarHem.RDS"))
-
-library(sqldf)
-
-matrixa = file(matrixa)
-matrixb = file(matrixb)
-matrixc = file(matrixc)
-
-matrix = sqldf("select * from matrixa union select * from matrixb union select * from matrixc", dbname = tempfile())
-
-saveRDS(matrix, file = path.expand("~/GSE97930_All_snDrop-seq_UMI_Count_Matrix_08-01-2017.txt.gz"))
+saveRDS(combinedmatrix, file = path.expand("~/GSE97930_All.RDS"))
