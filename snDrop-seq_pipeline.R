83f499f0d185cceb82bd11f4c6c491faa8189bc6
nshedd <58157630+nshedd@users.noreply.github.com>
2020-12-16 17:43:21 -0500

    Update snDrop-seq_pipeline.R

--- snDrop-seq_pipeline.R
+++ snDrop-seq_pipeline.R
@@ -4,38 +4,37 @@ library(patchwork)
 library(ggplot2)
 
 
-path1 = path.expand("~/GSE97930_VisualCortex_snDrop-seq_UMI_Count_Matrix_08-01-2017.txt.gz")
+path1 = path.expand("~/GSE97930_CerebellarHem_snDrop-seq_UMI_Count_Matrix_08-01-2017.txt.gz")
 
 matrix = read.table(path1, header=TRUE, row.names=1)
 
-visualcortex <- CreateSeuratObject(counts = matrix, project = "set2", min.cells = 3, min.features = 200)
+CerebellarHem <- CreateSeuratObject(counts = matrix, project = "set2", min.cells = 3, min.features = 200)
 
-visualcortex <- NormalizeData(visualcortex, normalization.method = "LogNormalize", scale.factor = 10000)
+CerebellarHem <- NormalizeData(CerebellarHem, normalization.method = "LogNormalize", scale.factor = 10000)
 
-visualcortex <- FindVariableFeatures(object = visualcortex)
+CerebellarHem <- FindVariableFeatures(object = CerebellarHem)
 
-all_cells <- rownames(visualcortex)
-visualcortex <- ScaleData(visualcortex, features = all_cells)
+all_cells <- rownames(CerebellarHem)
+CerebellarHem <- ScaleData(CerebellarHem, features = all_cells)
 
-visualcortex <- RunPCA(visualcortex, features = VariableFeatures(object = visualcortex))
+CerebellarHem <- RunPCA(CerebellarHem, features = VariableFeatures(object = CerebellarHem))
 
-visualcortex <- FindNeighbors(visualcortex, dims = 1:10)
-visualcortex <- FindClusters(visualcortex, resolution = 0.5)
+CerebellarHem <- FindNeighbors(CerebellarHem, dims = 1:10)
+CerebellarHem <- FindClusters(CerebellarHem, resolution = 0.5)
 
-visualcortex <- RunUMAP(visualcortex, dims = 1:10)
+CerebellarHem <- RunUMAP(CerebellarHem, dims = 1:10)
 
-new.cluster.ids <- c("Excitatory/Glutamatergic Neuron", "Neuron", "Oligodendrocyte", "GABAergic Neuron", "Glutamatergic Neuron", "Excitatory/Glutamatergic Neuron", 
-                     "Astrocyte", "Non-myelinating Schwann Cell", "Neuron", "Oligodendrocyte Precursor", "GABAergic/Glutamatergic Neuron", "Microglia", "Endothelial/Pericyte")
-names(new.cluster.ids) <- levels(visualcortex)
-visualcortex <- RenameIdents(visualcortex, new.cluster.ids)
+new.cluster.ids <- c("Neuron", "Neuron", "Neuron", "Neuron", "Astrocyte", "Neuron", "?", "Oligodendrocyte", "Microglia", "Neuron", "?", "?")
+names(new.cluster.ids) <- levels(CerebellarHem)
+CerebellarHem <- RenameIdents(CerebellarHem, new.cluster.ids)
 
-visualcortex.markers <- FindAllMarkers(visualcortex, only.pos = TRUE, min.pct = 0.25, thresh.use = 0.25)
-diff_expressed = visualcortex.markers %>% group_by(cluster)
+CerebellarHem.markers <- FindAllMarkers(CerebellarHem, only.pos = TRUE, min.pct = 0.25, thresh.use = 0.25)
+diff_expressed = CerebellarHem.markers %>% group_by(cluster)
 
-saveRDS(visualcortex, file = path.expand("~/GSE97930_visualcortex_snDrop-seq_UMI_Count_Matrix_Seurat.rds"))
+saveRDS(CerebellarHem, file = path.expand("~/GSE97930_CerebellarHem_snDrop-seq_UMI_Count_Matrix_Seurat.rds"))
 
-plot = DimPlot(visualcortex, reduction = "umap", label = TRUE, pt.size = 0.5) + NoLegend()
-ggsave(path.expand("~/umap_GSE97930_visualcortex_Seurat_default.png"), device=)
+plot = DimPlot(CerebellarHem, reduction = "umap", label = TRUE, pt.size = 0.5) + NoLegend()
+ggsave(path.expand("~/umap_GSE97930_CerebellarHem_Seurat_default.png"), device=)
 
 path2 = path.expand("~/Zlab single-cell marker genes - Brain.tsv")
 
@@ -54,4 +53,4 @@ for (gene in diff_expressed$gene) {
 
 diff_expressed$cell_type <- celltypes
 
-write.table(diff_expressed, file = path.expand("~/GSE97930_visualcortex_differentiallyexpressed.txt"), sep="\t")
+write.table(diff_expressed, file = path.expand("~/GSE97930_CerebellarHem_differentiallyexpressed.txt"), sep="\t")
