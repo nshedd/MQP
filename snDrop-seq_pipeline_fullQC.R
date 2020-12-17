e19e5bca3632a2898e1b1d87ec2289ceb195e161
nshedd <58157630+nshedd@users.noreply.github.com>
2020-12-17 10:49:19 -0500

    Update snDrop-seq_pipeline_fullQC.R

--- snDrop-seq_pipeline_fullQC.R
+++ snDrop-seq_pipeline_fullQC.R
@@ -42,10 +42,8 @@ CerebellarHem <- ScoreJackStraw(CerebellarHem, dims = 1:20)
 Jackstraw_CerebellarHem <- JackStrawPlot(CerebellarHem, dims = 1:15)
 ggsave(path.expand("~/Lake/CerebellarHem/jackstrawplot_GSE97930_CerebellarHem_Seurat.png"), device=)
 
-CerebellarHem <- FindNeighbors(CerebellarHem, dims = 1:10)
-CerebellarHem <- FindClusters(CerebellarHem, resolution = 0.5)
-
-CerebellarHem@active.ident <- plyr::mapvalues(x = CerebellarHem@active.ident)
+#CerebellarHem <- FindNeighbors(CerebellarHem, dims = 1:10)
+#CerebellarHem <- FindClusters(CerebellarHem, resolution = 0.5)
 
 CerebellarHem <- RunUMAP(CerebellarHem, dims = 1:10)
 
@@ -59,9 +57,9 @@ diff_expressed = CerebellarHem.markers %>% group_by(cluster)
 saveRDS(CerebellarHem, file = path.expand("~/Lake/CerebellarHem/GSE97930_CerebellarHem_snDrop-seq_UMI_Count_Matrix_Seurat.rds"))
 
 plot = DimPlot(CerebellarHem, reduction = "umap", label = TRUE, pt.size = 0.5) + NoLegend()
-ggsave(path.expand("~/Lake/CerebellarHem/umap_GSE97930_CerebellarHem_Seurat_default.png"), device=)
+ggsave(path.expand("~/Lake/CerebellarHem/umap_GSE97930_CerebellarHem_Seurat_origident.png"), device=)
 
-featureplot_CerebellarHem < - FeaturePlot(CerebellarHem, features = c("SYT1", "SYT1", "RBFOX3", "GAD2", "SLC1A3", "GRIN2B", "PCDH15", "MBP", "APBB1IP", "SLC6A1", "PCDH15", "SLC1A3"))
-ggsave(path.expand("~/Lake/CerebellarHem/featureplot_GSE97930_CerebellarHem_Seurat_default.png"), device=)
+#featureplot_CerebellarHem < - FeaturePlot(CerebellarHem, features = c("SYT1", "SYT1", "RBFOX3", "GAD2", "SLC1A3", "GRIN2B", "PCDH15", "MBP", "APBB1IP", "SLC6A1", "PCDH15", "SLC1A3"))
+#ggsave(path.expand("~/Lake/CerebellarHem/featureplot_GSE97930_CerebellarHem_Seurat_default.png"), device=)
   
-write.table(diff_expressed, file = path.expand("~/Lake/CerebellarHem/GSE97930_CerebellarHem_differentiallyexpressed.txt"), sep="\t")
+write.table(diff_expressed, file = path.expand("~/Lake/CerebellarHem/GSE97930_CerebellarHem_differentiallyexpressed_origident.txt"), sep="\t")
