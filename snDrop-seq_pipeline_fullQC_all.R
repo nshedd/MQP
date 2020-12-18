124040547c9280d4eafb84058efde20f2e7cf057
nshedd <58157630+nshedd@users.noreply.github.com>
2020-12-18 10:59:39 -0500

    Update snDrop-seq_pipeline_fullQC_all.R

--- snDrop-seq_pipeline_fullQC_all.R
+++ snDrop-seq_pipeline_fullQC_all.R
@@ -10,12 +10,12 @@ All <- CreateSeuratObject(counts = matrix, project = "set2", min.cells = 3, min.
 
 All[["percent.mt"]] <- PercentageFeatureSet(All, pattern = "^MT-")
 All_VlnPlot <- VlnPlot(All, features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol = 3)
-ggsave(path.expand("~/Lake/All/qcvlnplot_GSE97930_All_Seurat.png"), device=)
+ggsave(path.expand("~/Lake/All/qcvlnplot_GSE97930_All_Seurat.png"), device=, width = 21, height = 7)
 
 plot1 <- FeatureScatter(All, feature1 = "nCount_RNA", feature2 = "percent.mt")
 plot2 <- FeatureScatter(All, feature1 = "nCount_RNA", feature2 = "nFeature_RNA")
 plot <- plot1 + plot2
-ggsave(path.expand("~/Lake/All/featurescatter_GSE97930_All_Seurat.png"), device=)
+ggsave(path.expand("~/Lake/All/featurescatter_GSE97930_All_Seurat.png"), device, width = 14, height = 7
 
 All <- NormalizeData(All, normalization.method = "LogNormalize", scale.factor = 10000)
 
@@ -27,18 +27,21 @@ All <- ScaleData(All, features = all_cells)
 All <- RunPCA(All, features = VariableFeatures(object = All))
 
 pcs_plot_All <- VizDimLoadings(All, dims = 1:2, reduction = "pca")
-ggsave(path.expand("~/Lake/All/qcdimloadings_GSE97930_All_Seurat.png"), device=)
+ggsave(path.expand("~/Lake/All/qcdimloadings_GSE97930_All_Seurat.png"), device=, width = 14, height = 7)
 
 pca_plot_All <- DimPlot(All, reduction = "pca")
-ggsave(path.expand("~/Lake/All/pcascatter_GSE97930_All_Seurat.png"), device=)
+ggsave(path.expand("~/Lake/All/pcascatter_GSE97930_All_Seurat.png"), device=, width = 14, height = 7)
 
 pca_heatmap_All <- DimHeatmap(All, dims = 1, balanced = TRUE)
-ggsave(path.expand("~/Lake/All/pcaheatmap_GSE97930_All_Seurat.png"), plot=pca_heatmap_All, device=)
+ggsave(path.expand("~/Lake/All/pcaheatmap_GSE97930_All_Seurat.png"), plot=pca_heatmap_All, device=, width = 14, height = 7)
 
 All <- JackStraw(All, num.replicate = 100)
 All <- ScoreJackStraw(All, dims = 1:20)
 Jackstraw_All <- JackStrawPlot(All, dims = 1:15)
-ggsave(path.expand("~/Lake/All/jackstrawplot_GSE97930_All_Seurat.png"), device=)
+ggsave(path.expand("~/Lake/All/jackstrawplot_GSE97930_All_Seurat.png"), device=, width = 14, height = 7)
+
+Elbow_All<- ElbowPlot(All)
+ggsave(path.expand("~/Lake/All/elbowplot_GSE97930_All_Seurat.png"), device=, width = 14, height = 7)
 
 #All <- FindNeighbors(All, dims = 1:10)
 #All <- FindClusters(All, resolution = 0.5)
