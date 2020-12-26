bef433e0d642c3957f15e48d792d9a9aad9854cb
nshedd <58157630+nshedd@users.noreply.github.com>
2020-12-26 18:42:22 -0500

    Update snDrop-seq_pipeline_testPCs_CerebellarHem.R

--- snDrop-seq_pipeline_testPCs_CerebellarHem.R
+++ snDrop-seq_pipeline_testPCs_CerebellarHem.R
@@ -5,7 +5,7 @@ library(ggplot2)
 
 CerebellarHem = readRDS(file = path.expand("~/Lake/CerebellarHem/GSE97930_CerebellarHem_snDrop-seq_UMI_Count_Matrix_Seurat.rds"))
 
-CerebellarHem <- RunUMAP(CerebellarHem, dims = 2, metric="euclidean")
+CerebellarHem <- RunUMAP(CerebellarHem, dims = 1:2, metric="euclidean")
 
 #new.cluster.ids <- c("Neuron 1", "Neuron 2", "Neuron 3", "Neuron 4", "Astrocyte", "Neuron 5", "? 1", "Oligodendrocyte", "Microglia", "Neuron 6", "? 2", "? 3")
 #names(new.cluster.ids) <- levels(CerebellarHem)
