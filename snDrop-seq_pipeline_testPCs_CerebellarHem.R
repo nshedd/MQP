c3bb60ea18e481da4a50977b598077264bfa24f3
nshedd <58157630+nshedd@users.noreply.github.com>
2020-12-27 10:47:47 -0500

    Update snDrop-seq_pipeline_testPCs_CerebellarHem.R

--- snDrop-seq_pipeline_testPCs_CerebellarHem.R
+++ snDrop-seq_pipeline_testPCs_CerebellarHem.R
@@ -5,8 +5,8 @@ library(ggplot2)
 
 CerebellarHem = readRDS(file = path.expand("~/Lake/CerebellarHem/GSE97930_CerebellarHem_snDrop-seq_UMI_Count_Matrix_Seurat.rds"))
 
-CerebellarHem <- FindNeighbors(CerebellarHem, dims = 1:100)
-CerebellarHem <- FindClusters(CerebellarHem, resolution = 0.5)
+#CerebellarHem <- FindNeighbors(CerebellarHem, dims = 1:100)
+#CerebellarHem <- FindClusters(CerebellarHem, resolution = 0.5)
 
 CerebellarHem <- RunUMAP(CerebellarHem, dims = 1:100, metric="euclidean")
 
@@ -15,4 +15,4 @@ CerebellarHem <- RunUMAP(CerebellarHem, dims = 1:100, metric="euclidean")
 #CerebellarHem <- RenameIdents(CerebellarHem, new.cluster.ids)
 
 plot = DimPlot(CerebellarHem, reduction = "umap", label = TRUE, pt.size = 0.5) + NoLegend()
-ggsave(path.expand("~/Lake/CerebellarHem/umap_GSE97930_CerebellarHem_Seurat_100pc.png"), device=)
+ggsave(path.expand("~/Lake/CerebellarHem/umap_GSE97930_CerebellarHem_Seurat_100pc_oglabels.png"), device=)
