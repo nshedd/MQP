83f3aff01bb672cc128e066b9aa6db6bd65b4a15
nshedd <58157630+nshedd@users.noreply.github.com>
2020-12-27 09:27:07 -0500

    Update snDrop-seq_pipeline_testPCs_CerebellarHem.R

--- snDrop-seq_pipeline_testPCs_CerebellarHem.R
+++ snDrop-seq_pipeline_testPCs_CerebellarHem.R
@@ -5,7 +5,10 @@ library(ggplot2)
 
 CerebellarHem = readRDS(file = path.expand("~/Lake/CerebellarHem/GSE97930_CerebellarHem_snDrop-seq_UMI_Count_Matrix_Seurat.rds"))
 
-CerebellarHem <- RunUMAP(CerebellarHem, dims = 1:10, metric="euclidean")
+#CerebellarHem <- FindNeighbors(CerebellarHem, dims = 1:5)
+#CerebellarHem <- FindClusters(CerebellarHem, resolution = 0.5)
+
+CerebellarHem <- RunUMAP(CerebellarHem, dims = 1:5, metric="euclidean")
 
 #new.cluster.ids <- c("Neuron 1", "Neuron 2", "Neuron 3", "Neuron 4", "Astrocyte", "Neuron 5", "? 1", "Oligodendrocyte", "Microglia", "Neuron 6", "? 2", "? 3")
 #names(new.cluster.ids) <- levels(CerebellarHem)
@@ -14,7 +17,6 @@ CerebellarHem <- RunUMAP(CerebellarHem, dims = 1:10, metric="euclidean")
 CerebellarHem.markers <- FindAllMarkers(CerebellarHem, only.pos = TRUE, min.pct = 0.25, thresh.use = 0.25)
 diff_expressed = CerebellarHem.markers %>% group_by(cluster)
 
-saveRDS(CerebellarHem, file = path.expand("~/Lake/CerebellarHem/GSE97930_CerebellarHem_snDrop-seq_UMI_Count_Matrix_Seurat.rds"))
 
 plot = DimPlot(CerebellarHem, reduction = "umap", label = TRUE, pt.size = 0.5) + NoLegend()
-ggsave(path.expand("~/Lake/CerebellarHem/umap_GSE97930_CerebellarHem_Seurat_010pc.png"), device=)
+ggsave(path.expand("~/Lake/CerebellarHem/umap_GSE97930_CerebellarHem_Seurat_005p_oglabelsc.png"), device=)
