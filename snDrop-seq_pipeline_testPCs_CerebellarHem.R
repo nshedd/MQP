e397f315e94efd3f812970aa2d6c25674bd9ad50
nshedd <58157630+nshedd@users.noreply.github.com>
2020-12-27 11:46:43 -0500

    Update snDrop-seq_pipeline_testPCs_CerebellarHem.R

--- snDrop-seq_pipeline_testPCs_CerebellarHem.R
+++ snDrop-seq_pipeline_testPCs_CerebellarHem.R
@@ -5,14 +5,14 @@ library(ggplot2)
 
 CerebellarHem = readRDS(file = path.expand("~/Lake/CerebellarHem/GSE97930_CerebellarHem_snDrop-seq_UMI_Count_Matrix_Seurat.rds"))
 
-#CerebellarHem <- FindNeighbors(CerebellarHem, dims = 1:7)
-#CerebellarHem <- FindClusters(CerebellarHem, resolution = 0.5)
+CerebellarHem <- FindNeighbors(CerebellarHem, dims = 1:7)
+CerebellarHem <- FindClusters(CerebellarHem, resolution = 0.5)
 
 CerebellarHem <- RunUMAP(CerebellarHem, dims = 1:7, metric="euclidean")
 
-#new.cluster.ids <- c("Gran", "Purk/OPC1", "Ast", "Oli", "Mic", "End/Per/OPC2")
-#names(new.cluster.ids) <- levels(CerebellarHem)
-#CerebellarHem <- RenameIdents(CerebellarHem, new.cluster.ids)
+new.cluster.ids <- c("Gran1", "Gran2", "Gran3" "Ast", "Purk1", "Purk2", "OPC" "Oli", "Mic", "End/Per")
+names(new.cluster.ids) <- levels(CerebellarHem)
+CerebellarHem <- RenameIdents(CerebellarHem, new.cluster.ids)
 
 plot = DimPlot(CerebellarHem, reduction = "umap", label = TRUE, pt.size = 0.5) + NoLegend()
-ggsave(path.expand("~/Lake/CerebellarHem/umap_GSE97930_CerebellarHem_Seurat_007pc_oglabels.png"), device=)
+ggsave(path.expand("~/Lake/CerebellarHem/umap_GSE97930_CerebellarHem_Seurat_007pc.png"), device=)
