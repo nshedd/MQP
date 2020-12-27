42abc2fef50549f3af1f534b1e5adb91ac220989
nshedd <58157630+nshedd@users.noreply.github.com>
2020-12-27 11:27:15 -0500

    Update snDrop-seq_pipeline_testPCs_CerebellarHem.R

--- snDrop-seq_pipeline_testPCs_CerebellarHem.R
+++ snDrop-seq_pipeline_testPCs_CerebellarHem.R
@@ -15,4 +15,4 @@ CerebellarHem <- RunUMAP(CerebellarHem, dims = 1:200, metric="euclidean")
 #CerebellarHem <- RenameIdents(CerebellarHem, new.cluster.ids)
 
 plot = DimPlot(CerebellarHem, reduction = "umap", label = TRUE, pt.size = 0.5) + NoLegend()
-ggsave(path.expand("~/Lake/CerebellarHem/umap_GSE97930_CerebellarHem_Seurat_200pc.png"), device=)
+ggsave(path.expand("~/Lake/CerebellarHem/umap_GSE97930_CerebellarHem_Seurat_200pc_oglabels.png"), device=)
