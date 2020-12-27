3cbe4e8e8f5a5e9d7da091f0b90a925c857b7c27
nshedd <58157630+nshedd@users.noreply.github.com>
2020-12-27 09:41:44 -0500

    Update snDrop-seq_pipeline_testPCs_CerebellarHem.R

--- snDrop-seq_pipeline_testPCs_CerebellarHem.R
+++ snDrop-seq_pipeline_testPCs_CerebellarHem.R
@@ -10,13 +10,13 @@ CerebellarHem <- FindClusters(CerebellarHem, resolution = 0.5)
 
 CerebellarHem <- RunUMAP(CerebellarHem, dims = 1:5, metric="euclidean")
 
-new.cluster.ids <- c("Gran1", "Gran2", "Gran3", "Purk1", "Ast", "Purk2", "OPC", "Mic/End/Per")
-names(new.cluster.ids) <- levels(CerebellarHem)
-CerebellarHem <- RenameIdents(CerebellarHem, new.cluster.ids)
+#new.cluster.ids <- c("Gran1", "Gran2", "Gran3", "Purk1", "Ast", "Purk2", "OPC", "Mic/End/Per")
+#names(new.cluster.ids) <- levels(CerebellarHem)
+#CerebellarHem <- RenameIdents(CerebellarHem, new.cluster.ids)
 
 CerebellarHem.markers <- FindAllMarkers(CerebellarHem, only.pos = TRUE, min.pct = 0.25, thresh.use = 0.25)
 diff_expressed = CerebellarHem.markers %>% group_by(cluster)
 
 
 plot = DimPlot(CerebellarHem, reduction = "umap", label = TRUE, pt.size = 0.5) + NoLegend()
-ggsave(path.expand("~/Lake/CerebellarHem/umap_GSE97930_CerebellarHem_Seurat_005pc_labeled.png"), device=)
+ggsave(path.expand("~/Lake/CerebellarHem/umap_GSE97930_CerebellarHem_Seurat_005pc.png"), device=)
