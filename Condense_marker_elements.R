7e6a4e768c1306f8eb7eaa6b6786c40e557ab19e
nshedd <58157630+nshedd@users.noreply.github.com>
2021-01-04 20:37:55 -0500

    Update Condense_marker_elements.R

--- Condense_marker_elements.R
+++ Condense_marker_elements.R
@@ -7,8 +7,8 @@ for (i in 1:12) {
   temp_elements = marker_elements[marker_elements$group == i,]
   temp_elements_sorted = temp_elements[order(temp_elements$Log2FC, decreasing=TRUE),]
   j = nrow(temp_elements_sorted)
-  if (j >= 10) {
-    top_temp_elements = temp_elements_sorted[1:10,]
+  if (j >= 3) {
+    top_temp_elements = temp_elements_sorted[1:3,]
     top_10_elements = rbind(top_10_elements, top_temp_elements)
   }
   else if (j>0) {
@@ -19,4 +19,4 @@ for (i in 1:12) {
 
 top_10_elements = top_10_elements[-1,]
 
-write.table(top_10_elements, file = path.expand("~/pfc_marker_peaks_top10.txt"), sep = '\t')
+write.table(top_10_elements, file = path.expand("~/pfc_marker_peaks_top3.txt"), sep = '\t')
