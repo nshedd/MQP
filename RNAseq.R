bd2c30a3f05c7756f63061d4fbda6ab3c86de4eb
nshedd <58157630+nshedd@users.noreply.github.com>
2021-01-05 13:36:42 -0500

    Update RNAseq.R

--- RNAseq.R
+++ RNAseq.R
@@ -1,16 +1,16 @@
 library(Seurat)
 library(ggplot2)
 
-FrontalCortex <- readRDS(file = path.expand("~/Lake/FrontalCortex/GSE97930_FrontalCortex_snDrop-seq_UMI_Count_Matrix_Seurat.rds"))
+VisualCortex <- readRDS(file = path.expand("~/Lake/VisualCortex/GSE97930_VisualCortex_snDrop-seq_UMI_Count_Matrix_Seurat.rds"))
 
-FrontalCortex <- FindNeighbors(FrontalCortex, dims = 1:35)
-FrontalCortex <- FindClusters(FrontalCortex, resolution = 0.5)
+VisualCortex <- FindNeighbors(VisualCortex, dims = 1:44)
+VisualCortex <- FindClusters(VisualCortex, resolution = 0.5)
 
-FrontalCortex <- RunUMAP(FrontalCortex, dims = 1:35, metric="euclidean")
+VisualCortex <- RunUMAP(VisualCortex, dims = 1:44, metric="euclidean")
 
-new.cluster.ids <- c("End/Ex1/In1","Ex2","Oli","Ex3","In2","Ast","In3","Ex4","In4","OPC","Ex5","In5","Mic","Ex6","Ex7","In6","Ex8","Per")
-names(new.cluster.ids) <- levels(FrontalCortex)
-FrontalCortex <- RenameIdents(FrontalCortex, new.cluster.ids)
+#new.cluster.ids <- c("End/Ex1/In1","Ex2","Oli","Ex3","In2","Ast","In3","Ex4","In4","OPC","Ex5","In5","Mic","Ex6","Ex7","In6","Ex8","Per")
+#names(new.cluster.ids) <- levels(VisualCortex)
+#VisualCortex <- RenameIdents(VisualCortex, new.cluster.ids)
 
-plot = DimPlot(FrontalCortex, reduction = "umap", label = TRUE, pt.size = 0.5) + NoLegend()
-ggsave(path.expand("~/Lake/FrontalCortex/umap_GSE97930_FrontalCortex_Seurat_035pc.png"), device=)
+plot = DimPlot(VisualCortex, reduction = "umap", label = TRUE, pt.size = 0.5) + NoLegend()
+ggsave(path.expand("~/Lake/VisualCortex/umap_GSE97930_VisualCortex_Seurat_044pc.png"), device=)
