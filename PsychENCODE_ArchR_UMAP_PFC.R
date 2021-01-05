f3735e8a043af939fa365ffa9923375f7160744c
nshedd <58157630+nshedd@users.noreply.github.com>
2021-01-04 19:14:53 -0500

    Back to default parameters

--- PsychENCODE_ArchR_UMAP_PFC.R
+++ PsychENCODE_ArchR_UMAP_PFC.R
@@ -14,8 +14,6 @@ ArrowFiles = createArrowFiles( inputFiles = bam, sampleNames = key,
 proj = ArchRProject(ArrowFiles = ArrowFiles, outputDirectory = time, copyArrows = TRUE)
 rdhss = import("/data/projects/encode/Registry/V2/GRCh38/GRCh38-rDHSs.bed")
 
-print("made it here")
-
 proj = addPeakSet(ArchRProj = proj, peakSet = rdhss, force = FALSE)
 proj = addPeakMatrix(proj)
 
@@ -27,14 +25,14 @@ proj <- addUMAP(ArchRProj = proj, nNeighbors = 10, reducedDims = "IterativeLSI")
 
 p <- plotEmbedding(ArchRProj = proj, colorBy = "cellColData", name = "Clusters", embedding = "UMAP")		
 
-plotPDF(p, name = "Plot-UMAP-PFC-Clusters-nn10.pdf",		
+plotPDF(p, name = "Plot-UMAP-PFC-Clusters.pdf",		
          ArchRProj = proj, addDOC = FALSE, width = 5, height = 5)		
 
 markersPeaks <- getMarkerFeatures(ArchRProj = proj, useMatrix = "PeakMatrix", groupBy = "Clusters",
                                   bias = c("TSSEnrichment", "log10(nFrags)"),testMethod = "wilcoxon")
 
 markerList <- getMarkers(markersPeaks, cutOff = "FDR <= 0.01 & Log2FC >= 1")
-write.table(markerList, file = path.expand("~/git/MQP/PFC_Dev_Analysis/Tables/pfc_marker_peaks-nn10.txt"), sep = '\t')
+write.table(markerList, file = path.expand("~/git/MQP/PFC_Dev_Analysis/Tables/pfc_marker_peaks.txt"), sep = '\t')
 
 heatmapPeaks <- markerHeatmap(
   seMarker = markersPeaks, 
@@ -42,6 +40,6 @@ heatmapPeaks <- markerHeatmap(
   transpose = TRUE
 )
 draw(heatmapPeaks, heatmap_legend_side = "bot", annotation_legend_side = "bot")
-plotPDF(heatmapPeaks, name = "Peak-Marker-Heatmap-PFC-nn10", width = 8, height = 6, ArchRProj = proj, addDOC = FALSE)
+plotPDF(heatmapPeaks, name = "Peak-Marker-Heatmap-PFC", width = 8, height = 6, ArchRProj = proj, addDOC = FALSE)
 
 saveArchRProject(ArchRProj = proj)
