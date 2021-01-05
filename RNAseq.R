1712ea781e7d4a5188200ae4c7ba29f0768439fe
nshedd <58157630+nshedd@users.noreply.github.com>
2021-01-05 13:16:07 -0500

    Create RNAseq.R

---
+++ RNAseq.R
@@ -0,0 +1,15 @@
+library(Seurat)
+
+FrontalCortex <- readRDS(file = path.expand("~/Lake/FrontalCortex/GSE97930_FrontalCortex_snDrop-seq_UMI_Count_Matrix_Seurat.rds"))
+
+FrontalCortex <- FindNeighbors(FrontalCortex, dims = 1:35)
+FrontalCortex <- FindClusters(FrontalCortex, resolution = 0.5)
+
+FrontalCortex <- RunUMAP(FrontalCortex, dims = 1:2, metric="euclidean")
+
+#new.cluster.ids <- c("In1","Ex1","Ex2","End","Oli1","Ex3","Oli1","Ex4","Ex5","Per/Mic","OPC","Ex6","Ex7","In2","In3","Ast","Oli2")
+#names(new.cluster.ids) <- levels(FrontalCortex)
+#FrontalCortex <- RenameIdents(FrontalCortex, new.cluster.ids)
+
+plot = DimPlot(FrontalCortex, reduction = "umap", label = TRUE, pt.size = 0.5) + NoLegend()
+ggsave(path.expand("~/Lake/FrontalCortex/umap_GSE97930_FrontalCortex_Seurat_035pc.png"), device=)
