c3a72af53eea888950f3ba5083e3243397a8598e
nshedd <58157630+nshedd@users.noreply.github.com>
2021-01-13 17:11:39 -0500

    Create snDrop-seq_pipeline_identheatmap_VisualCortex.R

---
+++ snDrop-seq_pipeline_identheatmap_VisualCortex.R
@@ -0,0 +1,80 @@
+devtools::install_github("nshedd/scclusteval")
+
+library(dplyr)
+library(Seurat)
+library(patchwork)
+library(ggplot2)
+library(scclusteval)
+
+path1 = path.expand("~/GSE97930_VisualCortex_snDrop-seq_UMI_Count_Matrix_08-01-2017.txt.gz")
+
+matrix = read.table(path1, header=TRUE, row.names=1)
+
+VisualCortex <- CreateSeuratObject(counts = matrix, project = "set2", min.cells = 3, min.features = 200)		
+
+VisualCortex[["percent.mt"]] <- PercentageFeatureSet(VisualCortex, pattern = "^MT-")		
+
+VisualCortex <- subset(VisualCortex, subset = nFeature_RNA > 200 & nFeature_RNA < 2500 & percent.mt < 5)		
+
+VisualCortex <- NormalizeData(VisualCortex, normalization.method = "LogNormalize", scale.factor = 10000)		
+
+VisualCortex <- FindVariableFeatures(VisualCortex, selection.method = "vst", nfeatures = 2000)		
+
+all_cells <- rownames(VisualCortex)		
+VisualCortex <- ScaleData(VisualCortex, features = all_cells)		
+
+VisualCortex <- RunPCA(VisualCortex, features = VariableFeatures(object = VisualCortex))		
+
+saveRDS(VisualCortex, file = path.expand("~/Lake/VisualCortex/GSE97930_VisualCortex_snDrop-seq_UMI_Count_Matrix_Seurat.rds"))
+
+## Paper labels
+
+VisualCortex2 <- CreateSeuratObject(counts = matrix, project = "set2", min.cells = 3, min.features = 200)		
+
+VisualCortex2[["percent.mt"]] <- PercentageFeatureSet(VisualCortex2, pattern = "^MT-")		
+
+VisualCortex2 <- subset(VisualCortex2, subset = nFeature_RNA > 200 & nFeature_RNA < 2500 & percent.mt < 5)		
+
+VisualCortex2 <- NormalizeData(VisualCortex2, normalization.method = "LogNormalize", scale.factor = 10000)		
+
+VisualCortex2 <- FindVariableFeatures(VisualCortex2, selection.method = "vst", nfeatures = 2000)		
+
+all_cells <- rownames(VisualCortex2)		
+VisualCortex2 <- ScaleData(VisualCortex2, features = all_cells)		
+
+VisualCortex2 <- RunPCA(VisualCortex2, features = VariableFeatures(object = VisualCortex2))	
+
+VisualCortex2 <- readRDS(file = path.expand("~/Lake/VisualCortex/GSE97930_VisualCortex_snDrop-seq_UMI_Count_Matrix_Seurat.rds"))
+
+VisualCortex2 <- RunUMAP(VisualCortex, dims = 1:20, metric="euclidean")
+
+plot = DimPlot(VisualCortex2, reduction = "umap", label = TRUE, pt.size = 0.5) + NoLegend()
+ggsave(path.expand("~/Lake/VisualCortex/umap_GSE97930_VisualCortex_Seurat_findct_oglabels.png"), device=)
+
+## My labels
+VisualCortex <- FindNeighbors(VisualCortex, dims = 1:20)
+VisualCortex <- FindClusters(VisualCortex, resolution = 1)
+
+VisualCortex <- RunUMAP(VisualCortex, dims = 1:20, metric="euclidean")
+
+new.cluster.ids <- c("Ex1","Ex2","Oli1","Ast","Ex3","In1","Oli2","In2","In3","Ex4","OPC","Ex5",
+                     "Ex6","Ex7","In4","In5","Mic","Ex8","Ex9","Ex10","Ex11","Ex12","End/Per")
+names(new.cluster.ids) <- levels(VisualCortex)
+VisualCortex <- RenameIdents(VisualCortex, new.cluster.ids)
+
+plot = DimPlot(VisualCortex, reduction = "umap", label = TRUE, pt.size = 0.5) + NoLegend()
+ggsave(path.expand("~/Lake/VisualCortex/umap_GSE97930_VisualCortex_Seurat_findct.png"), device=)
+
+
+## Heatmap comparing labels
+
+oglabels <- Idents(VisualCortex2)
+oglabels <- factor(oglabels)
+
+newlabels <- Idents(VisualCortex)
+newlabels <- factor(newlabels)
+
+
+png(file=path.expand("~/Lake/VisualCortex/GSE97930_VisualCortex_Seurat_JaccardHeatmap.png"))
+PairWiseJaccardSetsHeatmap(ident1 = oglabels, ident2 = newlabels)
+dev.off()
