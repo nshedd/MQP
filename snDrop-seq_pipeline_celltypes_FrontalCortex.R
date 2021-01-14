7030973d15de317894d649bb2f1ad1c9c219d98d
nshedd <58157630+nshedd@users.noreply.github.com>
2021-01-14 01:39:11 -0500

    Update snDrop-seq_pipeline_celltypes_FrontalCortex.R

--- snDrop-seq_pipeline_celltypes_FrontalCortex.R
+++ snDrop-seq_pipeline_celltypes_FrontalCortex.R
@@ -4,14 +4,34 @@ library(patchwork)
 library(ggplot2)
 
 
-FrontalCortex <- readRDS(file = path.expand("~/Lake/FrontalCortex/GSE97930_FrontalCortex_snDrop-seq_UMI_Count_Matrix_Seurat.rds"))
+path1 = path.expand("~/GSE97930_FrontalCortex_snDrop-seq_UMI_Count_Matrix_08-01-2017.txt.gz")
 
-#new.cluster.ids <- c("Neuron 1", "Neuron 2", "Neuron 3", "Neuron 4", "Astrocyte", "Neuron 5", "? 1", "Oligodendrocyte", "Microglia", "Neuron 6", "? 2", "? 3")
-#names(new.cluster.ids) <- levels(FrontalCortex)
-#FrontalCortex <- RenameIdents(FrontalCortex, new.cluster.ids)
+matrix = read.table(path1, header=TRUE, row.names=1)
+
+FrontalCortex <- CreateSeuratObject(counts = matrix, project = "set2", min.cells = 3, min.features = 200)
+
+FrontalCortex[["percent.mt"]] <- PercentageFeatureSet(FrontalCortex, pattern = "^MT-")
+
+FrontalCortex <- subset(FrontalCortex, subset = nFeature_RNA > 200 & nFeature_RNA < 2500 & percent.mt < 5)
+
+FrontalCortex <- NormalizeData(FrontalCortex, normalization.method = "LogNormalize", scale.factor = 10000)
+
+FrontalCortex <- FindVariableFeatures(object = FrontalCortex)
+
+all_cells <- rownames(FrontalCortex)
+FrontalCortex <- ScaleData(FrontalCortex, features = all_cells)
+
+FrontalCortex <- RunPCA(FrontalCortex, features = VariableFeatures(object = FrontalCortex))
+
+saveRDS(FrontalCortex, file = path.expand("~/Lake/FrontalCortex/GSE97930_FrontalCortex_snDrop-seq_UMI_Count_Matrix_Seurat.rds"))
+
+FrontalCortex <- FindNeighbors(FrontalCortex, dims = 1:20)
+FrontalCortex <- FindClusters(FrontalCortex, resolution = 1)
+
+FrontalCortex <- RunUMAP(FrontalCortex, dims = 1:20, metric="euclidean")
 
 plot = DimPlot(FrontalCortex, reduction = "umap", label = TRUE, pt.size = 0.5) + NoLegend()
-ggsave(path.expand("~/Lake/FrontalCortex/umap_GSE97930_FrontalCortex_Seurat_findct.png"), device=)
+ggsave(path.expand("~/Lake/FrontalCortex/umap_GSE97930_FrontalCortex_Seurat_findct_1.png"), device=)
 
 FrontalCortex.markers <- FindAllMarkers(FrontalCortex, only.pos = TRUE, min.pct = 0.25, logfc.threshold = 0.25)
 FrontalCortex.markers %>% group_by(cluster)
