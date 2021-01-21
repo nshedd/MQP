2b9abdd3bd665e6ca0ca8a2edd895b3b41500df6
nshedd <58157630+nshedd@users.noreply.github.com>
2021-01-21 18:22:19 -0500

    Update snDrop-seq_pipeline_identheatmap_CerebellarHem.R

--- snDrop-seq_pipeline_identheatmap_CerebellarHem.R
+++ snDrop-seq_pipeline_identheatmap_CerebellarHem.R
@@ -27,14 +27,13 @@ CerebellarHem <- FindClusters(CerebellarHem, resolution = 0.5)
 
 CerebellarHem <- RunUMAP(CerebellarHem, dims = 1:20, metric="euclidean")
 
-ggsave(path.expand("~/Lake/CerebellarHem/umap_GSE97930_CerebellarHem_Seurat_findct.png"), device=)
-
 new.cluster.ids <- c("Gran1","Gran2","Gran3","Purk1","Ast1","Purk2","OPC1","Oli","Mic","End/Per","OPC2","Ast2")
 names(new.cluster.ids) <- levels(CerebellarHem)
 CerebellarHem <- RenameIdents(CerebellarHem, new.cluster.ids)
 
 plot = DimPlot(CerebellarHem, reduction = "umap", label = TRUE, pt.size = 0.5) + NoLegend()
 
+ggsave(path.expand("~/Lake/CerebellarHem/umap_GSE97930_CerebellarHem_Seurat_findct.png"), device=)
 
 ## Heatmap comparing labels
 
