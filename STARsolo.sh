54697ab6650c4a3b8df768c71981f7502a738ab7
nshedd <58157630+nshedd@users.noreply.github.com>
2021-02-16 23:49:43 -0500

    Update STARsolo.sh

--- STARsolo.sh
+++ STARsolo.sh
@@ -2,12 +2,20 @@
 work_path1=/data/zusers/sheddn
 work_path2=/data/zusers/pratth/sc/rna/mk
 
+
+cat "~/SCZ-BP/Mus_musculus.GRCm38.dna.primary_assembly.fa.gz" \
+    | sed -E 's/^>(\S+).*/>\1 \1/' \
+    | sed -E 's/^>([0-9]+|[XY]) />chr\1 /' \
+    | sed -E 's/^>MT />chrM /' \
+    > "~/SCZ-BP/Mus_musculus.GRCm38.dna.primary_assembly_modified.fa"
+
 # Build genome
 ~/STAR-2.7.7a/bin/Linux_x86_64/STAR \
 --runThreadN 1 \
 --runMode genomeGenerate \
 --genomeDir ~/SCZ-BP/ \
---genomeFastaFiles ~/SCZ-BP/Mus_musculus.GRCm38.dna.primary_assembly.fa.gz ~/SCZ-BP/gencode.vM23.primary_assembly.annotation.gtf.gz \
+--genomeFastaFiles ~/SCZ-BP/Mus_musculus.GRCm38.dna.primary_assembly_modified.fa 
+--sjdbGTFfile ~/SCZ-BP/gencode.vM23.primary_assembly.annotation.gtf.gz \
 
   
 # Run STARsolo 2.7.7a
