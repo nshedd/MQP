bc8a0dc0d8d55e81851d4a57d1b1dcf88df870ae
nshedd <58157630+nshedd@users.noreply.github.com>
2021-02-16 19:29:32 -0500

    Update STARsolo.sh

--- STARsolo.sh
+++ STARsolo.sh
@@ -4,13 +4,13 @@ work_path2=/data/zusers/pratth/sc/rna/mk
 
 
 # Then build index by STAR
-# sh test_build_index.sh 8 \
-#   ${work_path1}/SCZ-BP/mm10-2020-A_build/Mus_musculus.GRCm38.dna.primary_assembly.fa.modified \
-#   ${work_path1}/mm10-2020-A_build/gencode.vM23.primary_assembly.annotation.gtf.filtered \
-#   ${work_path1}/STAR_index_2.7.7a/
+sh test_build_index.sh 8 \
+  ${work_path1}/SCZ-BP/Mus_musculus.GRCm38.dna.primary_assembly.fa.gz \
+  ${work_path1}/SCZ-BP/gencode.vM23.primary_assembly.annotation.gtf.gz \
+  ${work_path1}/STAR_index_2.7.7a/
   
 # Run STARsolo 2.7.7a
-STAR \
+/data/zusers/sheddn/STAR-2.7.7a/bin/Linux_x86_64/STAR \
 --genomeDir ${work_path1}/STAR_index_2.7.7a/ \
 --readFilesIn ${work_path2}/190620KelA_D19-6787_1_sequence.fastq.gz ${work_path2}/190620KelA_D19-6787_2_sequence.fastq.gz \
 --soloType Droplet \
