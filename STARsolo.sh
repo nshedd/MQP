8e584bedfd1e30ee191ef6efe5805eb43ae9c376
nshedd <58157630+nshedd@users.noreply.github.com>
2021-02-18 17:34:55 -0500

    Update STARsolo.sh

--- STARsolo.sh
+++ STARsolo.sh
@@ -2,42 +2,11 @@
 work_path1=/data/zusers/sheddn
 work_path2=/data/zusers/pratth/sc/rna/mk
 
-genome="GRCh38"
-version="2020-A"
-
-build="GRCh38-2020-A_build"
-mkdir -p "$build"
-
-fasta_in="/home/sheddn/SCZ-BP/Homo_sapiens.GRCh38.dna.primary_assembly.fa"
-gtf_in="/home/sheddn/SCZ-BP/gencode.v32.primary_assembly.annotation.gtf"
-
-fasta_modified="$build/$(basename "$fasta_in").modified"
-
-ID="(ENS(MUS)?[GTE][0-9]+)\.([0-9]+)"
-cat "$fasta_in" \
-    | sed -E 's/^>(\S+).*/>\1 \1/' \
-    | sed -E 's/^>([0-9]+|[XY]) />chr\1 /' \
-    | sed -E 's/^>MT />chrM /' \
-    > "$fasta_modified"
-    
-# Create reference package
-/home/sheddn/yard/apps/cellranger-5.0.1/cellranger mkref --ref-version="$version" \
-    --genome="$genome" --fasta="$fasta_modified"
-
-# Build genome
-~/STAR-2.7.7a/bin/Linux_x86_64/STAR \
---runThreadN 1 \
---runMode genomeGenerate \
---genomeDir ~/SCZ-BP \
---genomeFastaFiles /home/sheddn/git/MQP/GRCh38-2020-A_build/Homo_sapiens.GRCh38.dna.primary_assembly.fa.modified \
---sjdbGTFfile /home/sheddn/git/MQP/GRCh38-2020-A_build/gencode.v32.primary_assembly.annotation.gtf.modified \
---outFileNamePrefix /home/sheddn/SCZ-BP \
-
   
 # Run STARsolo 2.7.7a
 ~/STAR-2.7.7a/bin/Linux_x86_64/STAR \
 --genomeDir ~/SCZ-BP/ \
---readFilesIn ${work_path2}/190620KelA_D19-6787_1_sequence.fastq.gz ${work_path2}/190620KelA_D19-6787_2_sequence.fastq.gz \
+--readFilesIn ${work_path2}/190620KelA_D19-6787_1_sequence.fastq ${work_path2}/190620KelA_D19-6787_2_sequence.fastq \
 --soloType Droplet \
 --soloCBwhitelist ~/737K-august-2016.txt \
 --runThreadN 16 \
@@ -46,3 +15,214 @@ cat "$fasta_in" \
 --outSAMunmapped Within \
 --outSAMattributes NH HI nM AS CR UR CB UB GX GN \
 --soloFeatures Gene
+--outFileNamePrefix ${work_path1}/SCZ-BP/190620KelA_D19/6787/
+
+# Run STARsolo 2.7.7a
+~/STAR-2.7.7a/bin/Linux_x86_64/STAR \
+--genomeDir ~/SCZ-BP/ \
+--readFilesIn ${work_path2}/190620KelA_D19-6788_1_sequence.fastq ${work_path2}/190620KelA_D19-6788_2_sequence.fastq \
+--soloType Droplet \
+--soloCBwhitelist ~/737K-august-2016.txt \
+--runThreadN 16 \
+--soloBarcodeReadLength 0 \
+--outSAMtype BAM SortedByCoordinate \
+--outSAMunmapped Within \
+--outSAMattributes NH HI nM AS CR UR CB UB GX GN \
+--soloFeatures Gene
+--outFileNamePrefix ${work_path1}/SCZ-BP/190620KelA_D19/6788/
+
+# Run STARsolo 2.7.7a
+~/STAR-2.7.7a/bin/Linux_x86_64/STAR \
+--genomeDir ~/SCZ-BP/ \
+--readFilesIn ${work_path2}/190620KelA_D19-6789_1_sequence.fastq ${work_path2}/190620KelA_D19-6789_2_sequence.fastq \
+--soloType Droplet \
+--soloCBwhitelist ~/737K-august-2016.txt \
+--runThreadN 16 \
+--soloBarcodeReadLength 0 \
+--outSAMtype BAM SortedByCoordinate \
+--outSAMunmapped Within \
+--outSAMattributes NH HI nM AS CR UR CB UB GX GN \
+--soloFeatures Gene
+--outFileNamePrefix ${work_path1}/SCZ-BP/190620KelA_D19/6789/
+
+# Run STARsolo 2.7.7a
+~/STAR-2.7.7a/bin/Linux_x86_64/STAR \
+--genomeDir ~/SCZ-BP/ \
+--readFilesIn ${work_path2}/190620KelA_D19-6790_1_sequence.fastq ${work_path2}/190620KelA_D19-6790_2_sequence.fastq \
+--soloType Droplet \
+--soloCBwhitelist ~/737K-august-2016.txt \
+--runThreadN 16 \
+--soloBarcodeReadLength 0 \
+--outSAMtype BAM SortedByCoordinate \
+--outSAMunmapped Within \
+--outSAMattributes NH HI nM AS CR UR CB UB GX GN \
+--soloFeatures Gene
+--outFileNamePrefix ${work_path1}/SCZ-BP/190620KelA_D19/6790/
+
+# Run STARsolo 2.7.7a
+~/STAR-2.7.7a/bin/Linux_x86_64/STAR \
+--genomeDir ~/SCZ-BP/ \
+--readFilesIn ${work_path2}/190620KelA_D19-6791_1_sequence.fastq ${work_path2}/190620KelA_D19-6791_2_sequence.fastq \
+--soloType Droplet \
+--soloCBwhitelist ~/737K-august-2016.txt \
+--runThreadN 16 \
+--soloBarcodeReadLength 0 \
+--outSAMtype BAM SortedByCoordinate \
+--outSAMunmapped Within \
+--outSAMattributes NH HI nM AS CR UR CB UB GX GN \
+--soloFeatures Gene
+--outFileNamePrefix ${work_path1}/SCZ-BP/190620KelA_D19/6791/
+
+# Run STARsolo 2.7.7a
+~/STAR-2.7.7a/bin/Linux_x86_64/STAR \
+--genomeDir ~/SCZ-BP/ \
+--readFilesIn ${work_path2}/190620KelA_D19-6792_1_sequence.fastq ${work_path2}/190620KelA_D19-6792_2_sequence.fastq \
+--soloType Droplet \
+--soloCBwhitelist ~/737K-august-2016.txt \
+--runThreadN 16 \
+--soloBarcodeReadLength 0 \
+--outSAMtype BAM SortedByCoordinate \
+--outSAMunmapped Within \
+--outSAMattributes NH HI nM AS CR UR CB UB GX GN \
+--soloFeatures Gene
+--outFileNamePrefix ${work_path1}/SCZ-BP/190620KelA_D19/6792/
+
+# Run STARsolo 2.7.7a
+~/STAR-2.7.7a/bin/Linux_x86_64/STAR \
+--genomeDir ~/SCZ-BP/ \
+--readFilesIn ${work_path2}/190620KelA_D19-6793_1_sequence.fastq ${work_path2}/190620KelA_D19-6793_2_sequence.fastq \
+--soloType Droplet \
+--soloCBwhitelist ~/737K-august-2016.txt \
+--runThreadN 16 \
+--soloBarcodeReadLength 0 \
+--outSAMtype BAM SortedByCoordinate \
+--outSAMunmapped Within \
+--outSAMattributes NH HI nM AS CR UR CB UB GX GN \
+--soloFeatures Gene
+--outFileNamePrefix ${work_path1}/SCZ-BP/190620KelA_D19/6793/
+
+# Run STARsolo 2.7.7a
+~/STAR-2.7.7a/bin/Linux_x86_64/STAR \
+--genomeDir ~/SCZ-BP/ \
+--readFilesIn ${work_path2}/190620KelA_D19-6794_1_sequence.fastq ${work_path2}/190620KelA_D19-6794_2_sequence.fastq \
+--soloType Droplet \
+--soloCBwhitelist ~/737K-august-2016.txt \
+--runThreadN 16 \
+--soloBarcodeReadLength 0 \
+--outSAMtype BAM SortedByCoordinate \
+--outSAMunmapped Within \
+--outSAMattributes NH HI nM AS CR UR CB UB GX GN \
+--soloFeatures Gene
+--outFileNamePrefix ${work_path1}/SCZ-BP/190620KelA_D19/6794/
+
+# Run STARsolo 2.7.7a
+~/STAR-2.7.7a/bin/Linux_x86_64/STAR \
+--genomeDir ~/SCZ-BP/ \
+--readFilesIn ${work_path2}/190620KelA_D19-6795_1_sequence.fastq ${work_path2}/190620KelA_D19-6795_2_sequence.fastq \
+--soloType Droplet \
+--soloCBwhitelist ~/737K-august-2016.txt \
+--runThreadN 16 \
+--soloBarcodeReadLength 0 \
+--outSAMtype BAM SortedByCoordinate \
+--outSAMunmapped Within \
+--outSAMattributes NH HI nM AS CR UR CB UB GX GN \
+--soloFeatures Gene
+--outFileNamePrefix ${work_path1}/SCZ-BP/190620KelA_D19/6795/
+
+# Run STARsolo 2.7.7a
+~/STAR-2.7.7a/bin/Linux_x86_64/STAR \
+--genomeDir ~/SCZ-BP/ \
+--readFilesIn ${work_path2}/190620KelA_D19-6796_1_sequence.fastq ${work_path2}/190620KelA_D19-6796_2_sequence.fastq \
+--soloType Droplet \
+--soloCBwhitelist ~/737K-august-2016.txt \
+--runThreadN 16 \
+--soloBarcodeReadLength 0 \
+--outSAMtype BAM SortedByCoordinate \
+--outSAMunmapped Within \
+--outSAMattributes NH HI nM AS CR UR CB UB GX GN \
+--soloFeatures Gene
+--outFileNamePrefix ${work_path1}/SCZ-BP/190620KelA_D19/6796/
+
+# Run STARsolo 2.7.7a
+~/STAR-2.7.7a/bin/Linux_x86_64/STAR \
+--genomeDir ~/SCZ-BP/ \
+--readFilesIn ${work_path2}/190620KelA_D19-6797_1_sequence.fastq ${work_path2}/190620KelA_D19-6797_2_sequence.fastq \
+--soloType Droplet \
+--soloCBwhitelist ~/737K-august-2016.txt \
+--runThreadN 16 \
+--soloBarcodeReadLength 0 \
+--outSAMtype BAM SortedByCoordinate \
+--outSAMunmapped Within \
+--outSAMattributes NH HI nM AS CR UR CB UB GX GN \
+--soloFeatures Gene
+--outFileNamePrefix ${work_path1}/SCZ-BP/190620KelA_D19/6797/
+
+# Run STARsolo 2.7.7a
+~/STAR-2.7.7a/bin/Linux_x86_64/STAR \
+--genomeDir ~/SCZ-BP/ \
+--readFilesIn ${work_path2}/190620KelA_D19-6798_1_sequence.fastq ${work_path2}/190620KelA_D19-6798_2_sequence.fastq \
+--soloType Droplet \
+--soloCBwhitelist ~/737K-august-2016.txt \
+--runThreadN 16 \
+--soloBarcodeReadLength 0 \
+--outSAMtype BAM SortedByCoordinate \
+--outSAMunmapped Within \
+--outSAMattributes NH HI nM AS CR UR CB UB GX GN \
+--soloFeatures Gene
+--outFileNamePrefix ${work_path1}/SCZ-BP/190620KelA_D19/6798/
+
+# Run STARsolo 2.7.7a
+~/STAR-2.7.7a/bin/Linux_x86_64/STAR \
+--genomeDir ~/SCZ-BP/ \
+--readFilesIn ${work_path2}/190620KelA_D19-6799_1_sequence.fastq ${work_path2}/190620KelA_D19-6799_2_sequence.fastq \
+--soloType Droplet \
+--soloCBwhitelist ~/737K-august-2016.txt \
+--runThreadN 16 \
+--soloBarcodeReadLength 0 \
+--outSAMtype BAM SortedByCoordinate \
+--outSAMunmapped Within \
+--outSAMattributes NH HI nM AS CR UR CB UB GX GN \
+--soloFeatures Gene
+--outFileNamePrefix ${work_path1}/SCZ-BP/190620KelA_D19/6799/
+
+# Run STARsolo 2.7.7a
+~/STAR-2.7.7a/bin/Linux_x86_64/STAR \
+--genomeDir ~/SCZ-BP/ \
+--readFilesIn ${work_path2}/190620KelA_D19-6800_1_sequence.fastq ${work_path2}/190620KelA_D19-6800_2_sequence.fastq \
+--soloType Droplet \
+--soloCBwhitelist ~/737K-august-2016.txt \
+--runThreadN 16 \
+--soloBarcodeReadLength 0 \
+--outSAMtype BAM SortedByCoordinate \
+--outSAMunmapped Within \
+--outSAMattributes NH HI nM AS CR UR CB UB GX GN \
+--soloFeatures Gene
+--outFileNamePrefix ${work_path1}/SCZ-BP/190620KelA_D19/6800/
+
+# Run STARsolo 2.7.7a
+~/STAR-2.7.7a/bin/Linux_x86_64/STAR \
+--genomeDir ~/SCZ-BP/ \
+--readFilesIn ${work_path2}/190620KelA_D19-6801_1_sequence.fastq ${work_path2}/190620KelA_D19-6801_2_sequence.fastq \
+--soloType Droplet \
+--soloCBwhitelist ~/737K-august-2016.txt \
+--runThreadN 16 \
+--soloBarcodeReadLength 0 \
+--outSAMtype BAM SortedByCoordinate \
+--outSAMunmapped Within \
+--outSAMattributes NH HI nM AS CR UR CB UB GX GN \
+--soloFeatures Gene
+--outFileNamePrefix ${work_path1}/SCZ-BP/190620KelA_D19/6801/
+
+# Run STARsolo 2.7.7a
+~/STAR-2.7.7a/bin/Linux_x86_64/STAR \
+--genomeDir ~/SCZ-BP/ \
+--readFilesIn ${work_path2}/190620KelA_D19-6802_1_sequence.fastq ${work_path2}/190620KelA_D19-6802_2_sequence.fastq \
+--soloType Droplet \
+--soloCBwhitelist ~/737K-august-2016.txt \
+--runThreadN 16 \
+--soloBarcodeReadLength 0 \
+--outSAMtype BAM SortedByCoordinate \
+--outSAMunmapped Within \
+--outSAMattributes NH HI nM AS CR UR CB UB GX GN \
+--soloFeatures Gene
+--outFileNamePrefix ${work_path1}/SCZ-BP/190620KelA_D19/6802/
