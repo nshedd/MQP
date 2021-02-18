17db928829ace9a633aace2877adf5dd40f61653
nshedd <58157630+nshedd@users.noreply.github.com>
2021-02-18 12:49:39 -0500

    Update StarSolo_UCLA-ASD.sh

--- StarSolo_UCLA-ASD.sh
+++ StarSolo_UCLA-ASD.sh
@@ -5,7 +5,7 @@ work_path2=/home/sheddn/UCLA-ASD/PEC_syn18898607/scRNAseq
 # Run STARsolo 2.7.7a
 ~/STAR-2.7.7a/bin/Linux_x86_64/STAR \
 --genomeDir ~/SCZ-BP/ \
---readFilesIn ${work_path2}/10BW_S13_L002_R1_001.fastq ${work_path2}/10BW_S13_L002_R2_001.fastq \
+--readFilesIn ${work_path2}/10BW_S13_L002_R1_001.fastq,${work_path2}/10BW_S13_L001_R1_001.fastq ${work_path2}/10BW_S13_L002_R2_001.fastq,${work_path2}/10BW_S13_L001_R2_001.fastq \
 --soloType Droplet \
 --soloCBwhitelist ~/737K-august-2016.txt \
 --runThreadN 16 \
