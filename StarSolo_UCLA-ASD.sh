0132ad879baa74ada7816f1d80b81614702a7ba2
nshedd <58157630+nshedd@users.noreply.github.com>
2021-02-18 12:21:12 -0500

    Update StarSolo_UCLA-ASD.sh

--- StarSolo_UCLA-ASD.sh
+++ StarSolo_UCLA-ASD.sh
@@ -1,11 +1,11 @@
 work_path1=/data/zusers/sheddn
-work_path2=/home/sheddn/UCLA-ASD/PEC_syn18898607/scRNAseq
+work_path2=/data/projects/psychencode/TR-SingleCell/UCLA-ASD/PEC_syn18898607/scRNAseq
 
   
 # Run STARsolo 2.7.7a
 ~/STAR-2.7.7a/bin/Linux_x86_64/STAR \
 --genomeDir ~/SCZ-BP/ \
---readFilesIn ${work_path2}/10BW_S13_L001_R1_001.fastq, ${work_path2}/10BW_S13_L001_R2_001.fastq  ${work_path2}/10BW_S13_L002_R1_001.fastq, ${work_path2}/10BW_S13_L002_R2_001.fastq\
+--readFilesIn ${work_path2}/10BW_S13_L001_R1_001.fastq.gz,${work_path2}/10BW_S13_L001_R2_001.fastq.gz  ${work_path2}/10BW_S13_L002_R1_001.fastq.gz,${work_path2}/10BW_S13_L002_R2_001.fastq.gz \
 --soloType Droplet \
 --soloCBwhitelist ~/737K-august-2016.txt \
 --runThreadN 16 \
