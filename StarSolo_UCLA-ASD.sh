c800f16d7efedbbed3c5862bca1ec7ec35bc9b71
nshedd <58157630+nshedd@users.noreply.github.com>
2021-02-18 10:35:41 -0500

    Create StarSolo_UCLA-ASD.sh

---
+++ StarSolo_UCLA-ASD.sh
@@ -0,0 +1,16 @@
+work_path1=/data/zusers/sheddn
+work_path2=/home/sheddn/UCLA-ASD/PEC_syn18898607/scRNAseq
+
+  
+# Run STARsolo 2.7.7a
+~/STAR-2.7.7a/bin/Linux_x86_64/STAR \
+--genomeDir ~/SCZ-BP/ \
+--readFilesIn ${work_path2}/10BW_S13_L001_R1_001.fastq ${work_path2}/10BW_S13_L001_R2_001.fastq \
+--soloType Droplet \
+--soloCBwhitelist ~/737K-august-2016.txt \
+--runThreadN 16 \
+--soloBarcodeReadLength 0 \
+--outSAMtype BAM SortedByCoordinate \
+--outSAMunmapped Within \
+--outSAMattributes NH HI nM AS CR UR CB UB GX GN \
+--soloFeatures Gene
