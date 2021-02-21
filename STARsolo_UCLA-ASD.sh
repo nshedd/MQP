94df297a26366cf16fd7d6ff701084c9b05098d3
nshedd <58157630+nshedd@users.noreply.github.com>
2021-02-20 23:42:49 -0500

    Update and rename StarSolo_UCLA-ASD.sh to STARsolo_UCLA-ASD.sh

---
+++ STARsolo_UCLA-ASD.sh
@@ -0,0 +1,17 @@
+work_path1=/data/zusers/sheddn/UCLA-ASD/data
+work_path2=/data/projects/psychencode/TR-SingleCell/UCLA-ASD/PEC_syn18898607/scRNAseq
+
+  
+# Run STARsolo 2.7.7a
+~/STAR-2.7.7a/bin/Linux_x86_64/STAR \
+--genomeDir ~/SCZ-BP/ \
+--readFilesIn <(gunzip -c ${work_path2}/10BW_S13_L001_R2_001.fastq.gz),<(gunzip -c ${work_path2}/10BW_S13_L002_R2_001.fastq.gz) <(gunzip -c ${work_path2}/10BW_S13_L001_R1_001.fastq.gz),<(gunzip -c ${work_path2}/10BW_S13_L002_R1_001.fastq.gz) \
+--soloType Droplet \
+--soloCBwhitelist ~/737K-august-2016.txt \
+--runThreadN 16 \
+--soloBarcodeReadLength 0 \
+--outSAMtype BAM SortedByCoordinate \
+--outSAMunmapped Within \
+--outSAMattributes NH HI nM AS CR UR CB UB GX GN \
+--soloFeatures Gene \
+--outFileNamePrefix ${work_path1}/10BW_S13/
