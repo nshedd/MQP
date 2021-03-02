1c464bcc83c2d48f6b31d71be9dd17d877f69e52
nshedd <58157630+nshedd@users.noreply.github.com>
2021-03-02 14:03:23 -0500

    Update Lake_FC_extract data.R

--- Lake_FC_extract data.R
+++ Lake_FC_extract data.R
@@ -18,15 +18,21 @@ FrontalCortex <- ScaleData(FrontalCortex, features = all_cells)
 
 FrontalCortex <- RunPCA(FrontalCortex, features = VariableFeatures(object = FrontalCortex))
 
-#FrontalCortex <- FindNeighbors(FrontalCortex, dims = 1:20)
-#FrontalCortex <- FindClusters(FrontalCortex, resolution = 1)
+FrontalCortex <- FindNeighbors(FrontalCortex, dims = 1:20)
+FrontalCortex <- FindClusters(FrontalCortex, resolution = 1)
 
 FrontalCortex <- RunUMAP(FrontalCortex, dims = 1:20)
 
 saveRDS(FrontalCortex,"~/Lake/FrontalCortex/Datavis_project.RDS")
 
-expression = GetAssayData(object = FrontalCortex, slot = "scale.data")
-write.table(expression, file = path.expand("~/Lake/FrontalCortex/datavis-expressionmatrix.txt"), sep="\t")
-
 embeddings = as.data.frame(pbmc[["umap"]]@cell.embeddings)
 write.table(embeddings, file = path.expand("~/Lake/FrontalCortex/datavis-embeddings.txt"), sep="\t")
+
+expression = GetAssayData(object = FrontalCortex, slot = "scale.data")
+
+marker_genes = c("SLC17A7", "SATB2", "GRIN1", "GRIN2B", "GAD1", "GAD2", "SLC6A1", "CLDN11", "MOG", "MOBP", "MBP", "SLC1A2", "SLC1A3", "SLC4A4", "GLUL", "AQP4"
+                "COBLL1", "DUSP1", "FLT1", "PCDH15", "OLIG1", "PCDH15", "OLIG1", "APBB1IP", "P2RY12", "APBB1IP", "P2RY12", "RYR1", "RELN", "GRM4", "RBFOX3")
+
+expression = expression[markergenes,]
+write.table(expression, file = path.expand("~/Lake/FrontalCortex/datavis-expressionmatrix.txt"), sep="\t")
+
