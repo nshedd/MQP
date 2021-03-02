eba234cc5f4b1501d99620751880b1c8ea58fe42
nshedd <58157630+nshedd@users.noreply.github.com>
2021-03-02 15:17:47 -0500

    No clustering

--- UCLA-ASD_CTL_UMAP_withHarmony.R
+++ UCLA-ASD_CTL_UMAP_withHarmony.R
@@ -24,15 +24,17 @@ library(harmony)
 
 # saveRDS(CTL, '/data/rusers/sheddn/UCLA-ASD/data/CTL_ScaledData.RDS')
 
-CTL <- readRDS('/data/rusers/sheddn/UCLA-ASD/data/CTL_PCAprocessed_BySample.RDS')
+# CTL <- readRDS('/data/rusers/sheddn/UCLA-ASD/data/CTL_PCAprocessed_BySample.RDS')
 
-CTL <- RunHarmony(CTL, "orig.ident")
+# CTL <- RunHarmony(CTL, "orig.ident")
 
-saveRDS(CTL, '/data/rusers/sheddn/UCLA-ASD/data/CTL_Harmonyprocessed_BySample.RDS')
+# saveRDS(CTL, '/data/rusers/sheddn/UCLA-ASD/data/CTL_Harmonyprocessed_BySample.RDS')
+
+CTL <- readRDS('/data/rusers/sheddn/UCLA-ASD/data/CTL_Harmonyprocessed_BySample.RDS')
 
 CTL <- RunUMAP(CTL, reduction = "harmony", dims = 1:30)
 
-CTL <- FindNeighbors(CTL, reduction = "harmony", dims = 1:30) %>% FindClusters()
+# CTL <- FindNeighbors(CTL, reduction = "harmony", dims = 1:30) %>% FindClusters()
 
 DimPlot(CTL, group.by="ident", label=TRUE, pt.size=0.5)
 ggsave('/data/rusers/sheddn/UCLA-ASD/plots/CTL-UMAP_Harmony.png')
