b149b90f10805f4df3e89fd5588d4bc55722892a
nshedd <58157630+nshedd@users.noreply.github.com>
2021-03-02 13:22:11 -0500

    Add PCA

--- UCLA-ASD_CTL_UMAP_withHarmony.R
+++ UCLA-ASD_CTL_UMAP_withHarmony.R
@@ -3,26 +3,28 @@ library(Seurat)
 library(ggplot2)
 library(harmony)
 
-CTL = readRDS('/data/rusers/sheddn/UCLA-ASD/data/CTL_SampleLabels')
+# CTL = readRDS('/data/rusers/sheddn/UCLA-ASD/data/CTL_SampleLabels')
 
-CTL[["percent.mt"]] <- PercentageFeatureSet(CTL, pattern = "^MT-")
+# CTL[["percent.mt"]] <- PercentageFeatureSet(CTL, pattern = "^MT-")
 
-VlnPlot(CTL, features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol = 3, )
-ggsave('/data/rusers/sheddn/UCLA-ASD/plots/CTL-VlnPlot.png', width = 14, height = 7)
+# VlnPlot(CTL, features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol = 3, )
+# ggsave('/data/rusers/sheddn/UCLA-ASD/plots/CTL-VlnPlot.png', width = 14, height = 7)
 
-plot1 <- FeatureScatter(CTL, feature1 = "nCount_RNA", feature2 = "percent.mt")
-plot2 <- FeatureScatter(CTL, feature1 = "nCount_RNA", feature2 = "nFeature_RNA")
-plot <- plot1 + plot2
-ggsave("/data/rusers/sheddn/UCLA-ASD/plots/FtrSctr.png", width = 14, height = 7)
+# plot1 <- FeatureScatter(CTL, feature1 = "nCount_RNA", feature2 = "percent.mt")
+# plot2 <- FeatureScatter(CTL, feature1 = "nCount_RNA", feature2 = "nFeature_RNA")
+# plot <- plot1 + plot2
+# ggsave("/data/rusers/sheddn/UCLA-ASD/plots/FtrSctr.png", width = 14, height = 7)
 
-CTL <- NormalizeData(CTL, normalization.method = "LogNormalize", scale.factor = 10000)
+# CTL <- NormalizeData(CTL, normalization.method = "LogNormalize", scale.factor = 10000)
 
-CTL <- FindVariableFeatures(CTL, selection.method = "vst", nfeatures = 2000)
+# CTL <- FindVariableFeatures(CTL, selection.method = "vst", nfeatures = 2000)
 
-all.genes <- rownames(CTL)
-CTL <- ScaleData(CTL, features = all.genes)
+# all.genes <- rownames(CTL)
+# CTL <- ScaleData(CTL, features = all.genes)
 
-saveRDS(CTL, '/data/rusers/sheddn/UCLA-ASD/data/CTL_ScaledData.RDS')
+# saveRDS(CTL, '/data/rusers/sheddn/UCLA-ASD/data/CTL_ScaledData.RDS')
+
+CTL <- readRDS('/data/rusers/sheddn/UCLA-ASD/data/CTL_PCAprocessed_BySample.RDS')
 
 CTL <- RunHarmony(CTL, group.by.vars = "Method")
 
