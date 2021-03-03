6126c95daf7729944ca0802055c08a425d098426
nshedd <58157630+nshedd@users.noreply.github.com>
2021-03-03 15:14:17 -0500

    Update UCLA-ASD_runUMAP_ASD.sh

--- UCLA-ASD_runUMAP_ASD.sh
+++ UCLA-ASD_runUMAP_ASD.sh
@@ -1,3 +1,3 @@
-Rscript /home/sheddn/git/MQP/UCLA-ASD_ASDL_CreateObjects.R
+Rscript /home/sheddn/git/MQP/UCLA-ASD_ASD_CreateObjects.R
 Rscript /home/sheddn/git/MQP/UCLA-ASD_ASD_UMAP.R
 Rscript /home/sheddn/git/MQP/UCLA-ASD_ASD_UMAP_withHarmony.R
