32ca7851cdf81b1bb58f10d459ececfc4c929209
nshedd <58157630+nshedd@users.noreply.github.com>
2021-03-03 00:00:01 -0500

    Update Lake_FC_extract_data.R

--- Lake_FC_extract_data.R
+++ Lake_FC_extract_data.R
@@ -27,7 +27,7 @@ embeddings = as.data.frame(FrontalCortex[["umap"]]@cell.embeddings)
 head(embeddings)
 embeddings$celltype = Idents(FrontalCortex)
 
-embeddings = as.data.frame(pbmc[["umap"]]@cell.embeddings)
+embeddings = as.data.frame(FrontalCortex[["umap"]]@cell.embeddings)
 write.table(embeddings, file = path.expand("/data/rusers/datavis4/embeddings.txt"), sep="\t")
 
 avg_expression_full = AverageExpression(FrontalCortex, group.by='ident')
