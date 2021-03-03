a4f36e93579c5356f24523215f1862cfb729999f
nshedd <58157630+nshedd@users.noreply.github.com>
2021-03-03 02:39:05 -0500

    Update Lake_FC_extract_data.R

--- Lake_FC_extract_data.R
+++ Lake_FC_extract_data.R
@@ -27,11 +27,11 @@ embeddings = as.data.frame(FrontalCortex[["umap"]]@cell.embeddings)
 head(embeddings)
 embeddings$celltype = Idents(FrontalCortex)
 
-write.table(embeddings, file = path.expand("/data/rusers/sheddn/datavis4/embeddings.txt"), sep="\t")
+write.table(embeddings, file = path.expand("/data/rusers/sheddn/datavis4/embeddings.txt"), sep=",")
 
 avg_expression_full = t(AverageExpression(FrontalCortex)[["RNA"]])
 
-write.table(avg_expression_full, file ="/data/rusers/sheddn/datavis4/expression_full.txt", sep=",")
+write.table(avg_expression_full, file ="/data/rusers/sheddn/datavis4/expression_full.txt", sep="\t")
 avg_expression_full = read.table("/data/rusers/sheddn/datavis4/expression_full.txt", header=TRUE, row.names=1)
 
 num_clusters = nrow(avg_expression_full)
