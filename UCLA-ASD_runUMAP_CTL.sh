8b298ea302773ef4a63f12b47c5f9b83a5cd8681
nshedd <58157630+nshedd@users.noreply.github.com>
2021-03-03 02:33:00 -0500

    Update UCLA-ASD_runUMAP_CTL.sh

--- UCLA-ASD_runUMAP_CTL.sh
+++ UCLA-ASD_runUMAP_CTL.sh
@@ -1,3 +1,3 @@
-# Rscript /home/sheddn/git/MQP/UCLA-ASD_CTL_CreateObjects.R
+Rscript /home/sheddn/git/MQP/UCLA-ASD_CTL_CreateObjects.R
 Rscript /home/sheddn/git/MQP/UCLA-ASD_CTL_UMAP.R
 Rscript /home/sheddn/git/MQP/UCLA-ASD_CTL_UMAP_withHarmony.R
