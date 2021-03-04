4cf435696b8f9f56e7b16d5850a6348e483643ba
nshedd <58157630+nshedd@users.noreply.github.com>
2021-03-04 13:28:57 -0500

    Update UCLA-ASD_CTL_UMAP_withHarmony.R

--- UCLA-ASD_CTL_UMAP_withHarmony.R
+++ UCLA-ASD_CTL_UMAP_withHarmony.R
@@ -3,6 +3,79 @@ library(Seurat)
 library(ggplot2)
 library(harmony)
 
+## BA4/6
+CTL = readRDS('/data/rusers/sheddn/UCLA-ASD/data/CTL_BA4.6')
+
+CTL[["percent.mt"]] <- PercentageFeatureSet(CTL, pattern = "^MT-")
+
+CTL <- NormalizeData(CTL, normalization.method = "LogNormalize", scale.factor = 10000)
+
+CTL <- FindVariableFeatures(CTL, selection.method = "vst", nfeatures = 2000)
+
+all.genes <- rownames(CTL)
+CTL <- ScaleData(CTL, features = all.genes)
+
+print("Saving Scaled data BA4.6...")
+saveRDS(CTL, '/data/rusers/sheddn/UCLA-ASD/data/CTL_ScaledData_BA4.6.RDS')
+
+CTL <- RunPCA(CTL, features = VariableFeatures(object = CTL))
+
+print("Saving PCA data BA4.6...")
+saveRDS(CTL, '/data/rusers/sheddn/UCLA-ASD/data/CTL_PCAprocessed_BA4.6.RDS')
+
+CTL <- RunHarmony(CTL, "orig.ident")
+
+print("Saving Harmony data BA4.6...")
+saveRDS(CTL, '/data/rusers/sheddn/UCLA-ASD/data/CTL_Harmonyprocessed_BA4.6.RDS')
+
+CTL <- RunUMAP(CTL, reduction = "harmony", dims = 1:30)
+
+CTL <- FindNeighbors(CTL, reduction = "harmony", dims = 1:30) %>% FindClusters()
+
+DimPlot(CTL, group.by="ident")
+ggsave('/data/rusers/sheddn/UCLA-ASD/plots/CTL-UMAP_Harmony_BA4.6.png', width = 8, height = 7)
+
+print("Saving UMAP data BA 46...")
+saveRDS(CTL, '/data/rusers/sheddn/UCLA-ASD/data/CTL_UMAPprocessed_BySample_Harmony_BA4.6.RDS')
+
+
+## BA9
+CTL = readRDS('/data/rusers/sheddn/UCLA-ASD/data/CTL_BA9')
+
+CTL[["percent.mt"]] <- PercentageFeatureSet(CTL, pattern = "^MT-")
+
+CTL <- NormalizeData(CTL, normalization.method = "LogNormalize", scale.factor = 10000)
+
+CTL <- FindVariableFeatures(CTL, selection.method = "vst", nfeatures = 2000)
+
+all.genes <- rownames(CTL)
+CTL <- ScaleData(CTL, features = all.genes)
+
+print("Saving Scaled data BA9...")
+saveRDS(CTL, '/data/rusers/sheddn/UCLA-ASD/data/CTL_ScaledData_BA9.RDS')
+
+CTL <- RunPCA(CTL, features = VariableFeatures(object = CTL))
+
+print("Saving PCA data BA9...")
+saveRDS(CTL, '/data/rusers/sheddn/UCLA-ASD/data/CTL_PCAprocessed_BA9.RDS')
+
+CTL <- RunHarmony(CTL, "orig.ident")
+
+print("Saving Harmony data BA9...")
+saveRDS(CTL, '/data/rusers/sheddn/UCLA-ASD/data/CTL_Harmonyprocessed_BA9.RDS')
+
+CTL <- RunUMAP(CTL, reduction = "harmony", dims = 1:30)
+
+CTL <- FindNeighbors(CTL, reduction = "harmony", dims = 1:30) %>% FindClusters()
+
+DimPlot(CTL, group.by="ident")
+ggsave('/data/rusers/sheddn/UCLA-ASD/plots/CTL-UMAP_Harmony_BA9.png', width = 8, height = 7)
+
+print("Saving UMAP data BA 46...")
+saveRDS(CTL, '/data/rusers/sheddn/UCLA-ASD/data/CTL_UMAPprocessed_BySample_Harmony_BA9.RDS')
+
+
+
 # CTL = readRDS('/data/rusers/sheddn/UCLA-ASD/data/CTL_SampleLabels')
 
 # CTL[["percent.mt"]] <- PercentageFeatureSet(CTL, pattern = "^MT-")
@@ -16,30 +89,30 @@ library(harmony)
 
 # saveRDS(CTL, '/data/rusers/sheddn/UCLA-ASD/data/CTL_ScaledData.RDS')
 
-CTL <- readRDS('/data/rusers/sheddn/UCLA-ASD/data/CTL_PCAprocessed_BySample.RDS')
+# CTL <- readRDS('/data/rusers/sheddn/UCLA-ASD/data/CTL_PCAprocessed_BySample.RDS')
 
-CTL <- RunHarmony(CTL, "orig.ident")
+# CTL <- RunHarmony(CTL, "orig.ident")
 
-saveRDS(CTL, '/data/rusers/sheddn/UCLA-ASD/data/CTL_Harmonyprocessed_BySample.RDS')
+# saveRDS(CTL, '/data/rusers/sheddn/UCLA-ASD/data/CTL_Harmonyprocessed_BySample.RDS')
 
 # CTL <- readRDS('/data/rusers/sheddn/UCLA-ASD/data/CTL_Harmonyprocessed_BySample.RDS')
 
-CTL <- RunUMAP(CTL, reduction = "harmony", dims = 1:30)
+# CTL <- RunUMAP(CTL, reduction = "harmony", dims = 1:30)
 
 # CTL <- FindNeighbors(CTL, reduction = "harmony", dims = 1:30) %>% FindClusters()
 
-DimPlot(CTL, group.by="ident")
-ggsave('/data/rusers/sheddn/UCLA-ASD/plots/CTL-UMAP_Harmony_samples.png', width = 8, height = 7)
+# DimPlot(CTL, group.by="ident")
+# ggsave('/data/rusers/sheddn/UCLA-ASD/plots/CTL-UMAP_Harmony_samples.png', width = 8, height = 7)
 
-saveRDS(CTL, '/data/rusers/sheddn/UCLA-ASD/data/CTL_UMAPprocessed_BySample_Harmony_OrigIdent.RDS')
+# saveRDS(CTL, '/data/rusers/sheddn/UCLA-ASD/data/CTL_UMAPprocessed_BySample_Harmony_OrigIdent.RDS')
 
-CTL <- readRDS('/data/rusers/sheddn/UCLA-ASD/data/CTL_Harmonyprocessed_BySample.RDS')
+# CTL <- readRDS('/data/rusers/sheddn/UCLA-ASD/data/CTL_Harmonyprocessed_BySample.RDS')
 
-CTL <- RunUMAP(CTL, reduction = "harmony", dims = 1:30)
+# CTL <- RunUMAP(CTL, reduction = "harmony", dims = 1:30)
 
-CTL <- FindNeighbors(CTL, reduction = "harmony", dims = 1:30) %>% FindClusters()
+# CTL <- FindNeighbors(CTL, reduction = "harmony", dims = 1:30) %>% FindClusters()
 
-DimPlot(CTL, group.by="ident", label=TRUE, pt.size=0.5)
-ggsave('/data/rusers/sheddn/UCLA-ASD/plots/CTL-UMAP_Harmony.png', width = 8, height = 7)
+# DimPlot(CTL, group.by="ident", label=TRUE, pt.size=0.5)
+# ggsave('/data/rusers/sheddn/UCLA-ASD/plots/CTL-UMAP_Harmony.png', width = 8, height = 7)
 
-saveRDS(CTL, '/data/rusers/sheddn/UCLA-ASD/data/CTL_UMAPprocessed_BySample_Harmony.RDS')
+# saveRDS(CTL, '/data/rusers/sheddn/UCLA-ASD/data/CTL_UMAPprocessed_BySample_Harmony.RDS')
