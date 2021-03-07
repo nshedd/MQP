574332dc5a0210a4c6b5ec830d4632b2106aa72a
nshedd <58157630+nshedd@users.noreply.github.com>
2021-03-07 12:14:02 -0500

    Update Lake_FC_extract_data.R

--- Lake_FC_extract_data.R
+++ Lake_FC_extract_data.R
@@ -37,7 +37,9 @@ expression = GetAssayData(object = FrontalCortex, slot = "scale.data")
 print("transposing...")
 expression = t(expression)
 print("done with transpose")
+head(expression)
 write.table(expression, file = path.expand("/data/rusers/sheddn/datavis4/FrontalCortex_expression.txt"), sep=",")
+print("done writing table first time")
 
 markergenes = c("SLC17A7", "SATB2", "GRIN1", "GRIN2B", "GAD1", "GAD2", "SLC6A1", "CLDN11", "MOG", "MOBP", "MBP", "SLC1A2", "SLC1A3", "SLC4A4", "GLUL", "AQP4",
                 "COBLL1", "DUSP1", "FLT1", "PCDH15", "OLIG1", "PCDH15", "OLIG1", "APBB1IP", "P2RY12", "APBB1IP", "P2RY12", "RYR1", "RELN", "GRM4", "RBFOX3")
