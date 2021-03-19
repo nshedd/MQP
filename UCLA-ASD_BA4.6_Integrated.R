c7eb3091d2d4bd15c1a4f204a1f1d9c096a6f8f0
nshedd <58157630+nshedd@users.noreply.github.com>
2021-03-19 17:01:51 -0400

    Update UCLA-ASD_BA4.6_Integrated.R

--- UCLA-ASD_BA4.6_Integrated.R
+++ UCLA-ASD_BA4.6_Integrated.R
@@ -20,34 +20,34 @@ library(DoubletFinder)
 
 # saveRDS(BA4.6, '/data/rusers/sheddn/UCLA-ASD/data/combined_BA4.6')
 
-BA4.6 <- readRDS('/data/rusers/sheddn/UCLA-ASD/data/combined_BA4.6')
+# BA4.6 <- readRDS('/data/rusers/sheddn/UCLA-ASD/data/combined_BA4.6')
 
-all.genes <- rownames(BA4.6)
-BA4.6 <- ScaleData(BA4.6, features = all.genes)
+# all.genes <- rownames(BA4.6)
+# BA4.6 <- ScaleData(BA4.6, features = all.genes)
 
-BA4.6 <- RunPCA(BA4.6, features = VariableFeatures(object = BA4.6))
+# BA4.6 <- RunPCA(BA4.6, features = VariableFeatures(object = BA4.6))
 
-BA4.6 <- RunHarmony(BA4.6, "orig.ident")
+# BA4.6 <- RunHarmony(BA4.6, "orig.ident")
 
-BA4.6 <- RunUMAP(BA4.6, reduction = "harmony", dims = 1:20)
+# BA4.6 <- RunUMAP(BA4.6, reduction = "harmony", dims = 1:20)
 
-BA4.6 <- FindNeighbors(BA4.6, reduction = "harmony", dims = 1:20) %>% FindClusters(resolution=0.5)
+# BA4.6 <- FindNeighbors(BA4.6, reduction = "harmony", dims = 1:20) %>% FindClusters(resolution=0.5)
 
 ## pK indetification
-sweep.res.list_pbmc <- paramSweep_v3(BA4.6, PCs = 1:20, sct = FALSE)
-sweep.stats_pbmc <- summarizeSweep(sweep.res.list_pbmc, GT = FALSE)
-bcmvn_pbmc <- find.pK(sweep.stats_pbmc)
-bcmvn_pbmc$pK <- as.numeric(as.character(bcmvn_pbmc$pK))
+# sweep.res.list_pbmc <- paramSweep_v3(BA4.6, PCs = 1:20, sct = FALSE)
+# sweep.stats_pbmc <- summarizeSweep(sweep.res.list_pbmc, GT = FALSE)
+# bcmvn_pbmc <- find.pK(sweep.stats_pbmc)
+# bcmvn_pbmc$pK <- as.numeric(as.character(bcmvn_pbmc$pK))
 
 ## Doublet proportion estimate
-annotations <- BA4.6@meta.data$seurat_clusters
-homotypic.prop <- modelHomotypic(annotations)           ## ex: annotations <- seu_pbmc@meta.data$ClusteringResults
-nExp_poi <- round(0.15*nrow(BA4.6@meta.data))  ## Assuming 15% doublet formation rate - tailor for your dataset
-nExp_poi.adj <- round(nExp_poi*(1-homotypic.prop))
+# annotations <- BA4.6@meta.data$seurat_clusters
+# homotypic.prop <- modelHomotypic(annotations)           ## ex: annotations <- seu_pbmc@meta.data$ClusteringResults
+# nExp_poi <- round(0.15*nrow(BA4.6@meta.data))  ## Assuming 15% doublet formation rate - tailor for your dataset
+# nExp_poi.adj <- round(nExp_poi*(1-homotypic.prop))
 
-BA4.6 <- doubletFinder_v3(BA4.6, PCs = 1:20, pN = 0.15, pK = bcmvn_pbmc$pK[which.max(bcmvn_pbmc$BCmetric)], nExp = nExp_poi, reuse.pANN = FALSE, sct = FALSE)
+# BA4.6 <- doubletFinder_v3(BA4.6, PCs = 1:20, pN = 0.15, pK = bcmvn_pbmc$pK[which.max(bcmvn_pbmc$BCmetric)], nExp = nExp_poi, reuse.pANN = FALSE, sct = FALSE)
 
-saveRDS(BA4.6, '/data/rusers/sheddn/UCLA-ASD/data/combined_BA4.6_DoubletsRemoved')
+# saveRDS(BA4.6, '/data/rusers/sheddn/UCLA-ASD/data/combined_BA4.6_DoubletsRemoved')
 
 BA4.6 <- readRDS('/data/rusers/sheddn/UCLA-ASD/data/combined_BA4.6_DoubletsRemoved')
 
