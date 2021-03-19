3fd0eaa6d052f9f24e22731e0b6b2887e9382737
nshedd <58157630+nshedd@users.noreply.github.com>
2021-03-19 07:58:07 -0400

    Update UCLA-ASD_BA4.6_Integrated.R

--- UCLA-ASD_BA4.6_Integrated.R
+++ UCLA-ASD_BA4.6_Integrated.R
@@ -5,22 +5,22 @@ library(harmony)
 library(DoubletFinder)
 
 
-ASD_BA4.6 <- readRDS('/data/rusers/sheddn/UCLA-ASD/data/ASD_BA4.6')
-ASD_BA4.6$Group <- "ASD"
+# ASD_BA4.6 <- readRDS('/data/rusers/sheddn/UCLA-ASD/data/ASD_BA4.6')
+# ASD_BA4.6$Group <- "ASD"
 
 
-CTL_BA4.6 <- readRDS('/data/rusers/sheddn/UCLA-ASD/data/CTL_BA4.6')
-CTL_BA4.6$Group <- "CTL"
+# CTL_BA4.6 <- readRDS('/data/rusers/sheddn/UCLA-ASD/data/CTL_BA4.6')
+# CTL_BA4.6$Group <- "CTL"
 
-BA4.6 <- merge(CTL_BA4.6, y=ASD_BA4.6,add.cell.ids=c('CTL','ASD'),project = "UCLA-ASD")
+# BA4.6 <- merge(CTL_BA4.6, y=ASD_BA4.6,add.cell.ids=c('CTL','ASD'),project = "UCLA-ASD")
 
-BA4.6 <- NormalizeData(BA4.6, normalization.method = "LogNormalize", scale.factor = 10000)
-BA4.6 <- FindVariableFeatures(BA4.6, selection.method = "vst", nfeatures = 2000)
+# BA4.6 <- NormalizeData(BA4.6, normalization.method = "LogNormalize", scale.factor = 10000)
+# BA4.6 <- FindVariableFeatures(BA4.6, selection.method = "vst", nfeatures = 2000)
 
 
-saveRDS(BA4.6, '/data/rusers/sheddn/UCLA-ASD/data/combined_BA4.6')
+# saveRDS(BA4.6, '/data/rusers/sheddn/UCLA-ASD/data/combined_BA4.6')
 
-# BA4.6 <- readRDS('/data/rusers/sheddn/UCLA-ASD/data/combined_BA4.6')
+BA4.6 <- readRDS('/data/rusers/sheddn/UCLA-ASD/data/combined_BA4.6')
 
 all.genes <- rownames(BA4.6)
 BA4.6 <- ScaleData(BA4.6, features = all.genes)
@@ -31,7 +31,7 @@ BA4.6 <- RunHarmony(BA4.6, "orig.ident")
 
 BA4.6 <- RunUMAP(BA4.6, reduction = "harmony", dims = 1:20)
 
-BA4.6 <- FindNeighbors(CTL_BA4.6, reduction = "harmony", dims = 1:20) %>% FindClusters(resolution=0.5)
+BA4.6 <- FindNeighbors(BA4.6, reduction = "harmony", dims = 1:20) %>% FindClusters(resolution=0.5)
 
 ## pK indetification
 sweep.res.list_pbmc <- paramSweep_v3(BA4.6, PCs = 1:20, sct = FALSE)
@@ -57,15 +57,15 @@ ggsave('/data/rusers/sheddn/UCLA-ASD/plots/UMAP_Harmony_BA4.6_integrated_separat
 
 saveRDS(BA4.6, '/data/rusers/sheddn/UCLA-ASD/data/combined_BA4.6_DoubletsRemoved')
 
-CTL.markers <- FindAllMarkers(CTL, only.pos = TRUE, min.pct = 0.25, logfc.threshold = 0.25)
-CTL.markers %>% group_by(cluster)
+BA4.6.markers <- FindAllMarkers(BA4.6, only.pos = TRUE, min.pct = 0.25, logfc.threshold = 0.25)
+BA4.6.markers %>% group_by(cluster)
 
 marker_gene_table = read.table(path.expand("~/Zlab single-cell marker genes - Brain 3.tsv"), header=TRUE, sep="\t")
 all_known_marker_genes = marker_gene_table$Human.Gene
 
-intersection = intersect(CTL.markers$gene, all_known_marker_genes)
+intersection = intersect(BA4.6.markers$gene, all_known_marker_genes)
 
-dotplot <- DotPlot(CTL, features = intersection) +
+dotplot <- DotPlot(BA4.6, features = intersection) +
   theme(axis.text.x = element_text(angle = 90)) +
-  scale_y_discrete(limits = rev(levels(CTL$seurat_clusters)))
-ggsave("/data/rusers/sheddn/UCLA-ASD/plots/CTL_BA9_dotplot.png", width = 14, height = 7)
+  scale_y_discrete(limits = rev(levels(BA4.6$seurat_clusters)))
+ggsave("/data/rusers/sheddn/UCLA-ASD/plots/combined_BA4.6_dotplot.png", width = 14, height = 7)
