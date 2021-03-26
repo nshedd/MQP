4980b44b1a44d28c4b45a92956bbf60c92494554
nshedd <58157630+nshedd@users.noreply.github.com>
2021-03-26 16:39:57 -0400

    remove gsubexpression

--- Lake_ArchR_analysis.R
+++ Lake_ArchR_analysis.R
@@ -1,9 +1,4 @@
-if (!requireNamespace("devtools", quietly = TRUE)) install.packages("devtools")
-if (!requireNamespace("BiocManager", quietly = TRUE)) install.packages("BiocManager")
-devtools::install_github("GreenleafLab/ArchR", ref="master", repos = BiocManager::repositories())
-
 library(ArchR)
-ArchR::installExtraPackages()
 
 addArchRGenome("hg38")
 
@@ -14,8 +9,7 @@ bam = "/data/zusers/pratth/sc/fragments.lake.tsv.gz"
 key = "Lake"
 
 ArrowFiles = createArrowFiles( inputFiles = bam, sampleNames = key,
-  filterTSS = 4, filterFrags = 1000, addTileMat = TRUE, addGeneScoreMat = TRUE, 
-  gsubExpression=":.*")
+  filterTSS = 4, filterFrags = 1000, addTileMat = TRUE, addGeneScoreMat = TRUE)
 
 proj = ArchRProject(ArrowFiles = ArrowFiles, outputDirectory = time, copyArrows = TRUE)
 rdhss = import("/data/projects/encode/Registry/V2/GRCh38/GRCh38-rDHSs.bed")
