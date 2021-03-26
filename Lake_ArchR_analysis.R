65d143102007fb7e850f4bfdc868cb5977e7fa65
nshedd <58157630+nshedd@users.noreply.github.com>
2021-03-26 16:28:07 -0400

    Create Lake_ArchR_analysis.R

---
+++ Lake_ArchR_analysis.R
@@ -0,0 +1,21 @@
+if (!requireNamespace("devtools", quietly = TRUE)) install.packages("devtools")
+if (!requireNamespace("BiocManager", quietly = TRUE)) install.packages("BiocManager")
+devtools::install_github("GreenleafLab/ArchR", ref="master", repos = BiocManager::repositories())
+
+library(ArchR)
+ArchR::installExtraPackages()
+
+addArchRGenome("hg38")
+
+args <- commandArgs(trailingOnly = TRUE)
+time = "Lake_Integration_Analysis"
+print(time)
+bam = "/data/zusers/pratth/sc/fragments.lake.tsv.gz"
+key = "Lake"
+
+ArrowFiles = createArrowFiles( inputFiles = bam, sampleNames = key,
+  filterTSS = 4, filterFrags = 1000, addTileMat = TRUE, addGeneScoreMat = TRUE, 
+  gsubExpression=":.*")
+
+proj = ArchRProject(ArrowFiles = ArrowFiles, outputDirectory = time, copyArrows = TRUE)
+rdhss = import("/data/projects/encode/Registry/V2/GRCh38/GRCh38-rDHSs.bed")
