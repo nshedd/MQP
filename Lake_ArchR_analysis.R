083c5fe3eab724746104a644fbfafa92f20e3af7
nshedd <58157630+nshedd@users.noreply.github.com>
2021-03-26 16:53:35 -0400

    Update Lake_ArchR_analysis.R

--- Lake_ArchR_analysis.R
+++ Lake_ArchR_analysis.R
@@ -5,10 +5,15 @@ addArchRGenome("hg38")
 args <- commandArgs(trailingOnly = TRUE)
 time = "/data/rusers/sheddn/Lake_ths-seq/Lake_Integration_Analysis"
 print(time)
-bam = "/data/zusers/pratth/sc/fragments.lake.tsv.gz"
+fragment = "/data/zusers/pratth/sc/fragments.lake.tsv.gz"
 key = "Lake"
 
-ArrowFiles = createArrowFiles( inputFiles = bam, sampleNames = key,
+reformatFragmentFiles(
+  fragmentFiles = fragment,
+  checkChrPrefix = getArchRChrPrefix()
+)
+
+ArrowFiles = createArrowFiles( inputFiles = fragment, sampleNames = key,
   filterTSS = 4, filterFrags = 1000, addTileMat = TRUE, addGeneScoreMat = TRUE)
 
 proj = ArchRProject(ArrowFiles = ArrowFiles, outputDirectory = time, copyArrows = TRUE)
