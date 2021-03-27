67b6288652d1ecc8b9e98e92cf3ade3b7298f0e4
nshedd <58157630+nshedd@users.noreply.github.com>
2021-03-27 09:58:08 -0400

    Update UCLA-ASD_integrated_SingleR.R

--- UCLA-ASD_integrated_SingleR.R
+++ UCLA-ASD_integrated_SingleR.R
@@ -6,6 +6,22 @@ library(DoubletFinder)
 library(SingleR)
 library(SingleCellExperiment)
 
+## Load SingleR reference dataset
+path1 = path.expand("~/GSE97930_FrontalCortex_snDrop-seq_UMI_Count_Matrix_08-01-2017.txt.gz")
+
+matrix = read.table(path1, header=TRUE, row.names=1)
+Lake <- CreateSeuratObject(counts = matrix, project = "SeuratPipeline", min.cells = 3, min.features = 200)
+
+Lake[["percent.mt"]] <- PercentageFeatureSet(Lake, pattern = "^MT-")
+
+Lake <- subset(Lake, subset = nFeature_RNA > 200 & nFeature_RNA < 3000)
+
+Lake <- NormalizeData(Lake, normalization.method = "LogNormalize", scale.factor = 10000)
+
+Lake_SCE <- as.SingleCellExperiment(Lake)
+Lake_labels <- Idents(Lake)
+
+
 BA4.6 <- readRDS('/data/rusers/sheddn/UCLA-ASD/data/combined_BA4.6_WithDEGs.RDS')
 
 BA4.6_SCE <- as.SingleCellExperiment(BA4.6)
