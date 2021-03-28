8983e5e6d1585f51f34dfe9126aab40c752b97eb
nshedd <58157630+nshedd@users.noreply.github.com>
2021-03-28 10:55:53 -0400

    Update UCLA-ASD_integrated_SingleR.R

--- UCLA-ASD_integrated_SingleR.R
+++ UCLA-ASD_integrated_SingleR.R
@@ -19,28 +19,31 @@ Lake <- subset(Lake, subset = nFeature_RNA > 200 & nFeature_RNA < 3000)
 Lake <- NormalizeData(Lake, normalization.method = "LogNormalize", scale.factor = 10000)
 
 Lake_SCE <- as.SingleCellExperiment(Lake)
-Lake_labels <- Idents(Lake)
+Lake_labels <- Lake$orig.ident
 
 
 BA4.6 <- readRDS('/data/rusers/sheddn/UCLA-ASD/data/combined_BA4.6_WithDEGs.RDS')
 
-BA4.6_SCE <- as.SingleCellExperiment(BA4.6)
-BA4.6_clust <- Idents(BA4.6)
-
 print('Running SingleR...')
-BA4.6_SingleR <- SingleR(test=BA4.6_SCE,
+BA4.6_SingleR <- SingleR(test=GetAssayData(BA4.6, assay = "RNA"),
                        ref=Lake_SCE,
+                       method='cluster',
                        labels=Lake_labels,
-                       clusters=BA4.6_clust,
+                       clusters=Idents(BA4.6),
                        assay.type.test = "logcounts",
                        assay.type.ref = "logcounts")
 
 print('Plotting...')
 BA4.6$SingleR.pruned.calls <- BA4.6_SingleR$pruned.labels
 BA4.6$SingleR.calls <- BA4.6_SingleR$labels
-BA4.6$SingleR.cluster <- BA4.6_SingleR$cluster
+print(BA4.6_SingleR$cluster.labels)
 
-DimPlot(BA4.6, group.by="SingleR.cluster", label=TRUE, pt.size=0.5)
+new.cluster.ids <- BA4.6_SingleR$cluster.labels
+names(new.cluster.ids) <- levels(BA4.6)
+BA4.6 <- RenameIdents(BA4.6, new.cluster.ids)
+
+
+DimPlot(BA4.6, label=TRUE, pt.size=0.5)
 ggsave('/data/rusers/sheddn/UCLA-ASD/plots/UMAP_Harmony_BA4.6_integrated_SingleRlabel.png', width = 8, height = 7)
 
 saveRDS(BA4.6, '/data/rusers/sheddn/UCLA-ASD/data/combined_BA4.6_SingleR.RDS')
@@ -48,23 +51,25 @@ saveRDS(BA4.6, '/data/rusers/sheddn/UCLA-ASD/data/combined_BA4.6_SingleR.RDS')
 
 BA9 <- readRDS('/data/rusers/sheddn/UCLA-ASD/data/combined_BA9_WithDEGs.RDS')
 
-BA9_SCE <- as.SingleCellExperiment(BA9)
-BA9_clust <- Idents(BA9)
-
 print('Running SingleR...')
-BA9_SingleR <- SingleR(test=BA9_SCE,
-                         ref=Lake_SCE,
-                         labels=Lake_labels,
-                         clusters=BA9_clust,
-                         assay.type.test = "logcounts",
-                         assay.type.ref = "logcounts")
+BA9_SingleR <- SingleR(test=GetAssayData(BA9, assay = "RNA"),
+                       ref=Lake_SCE,
+                       method='cluster',
+                       labels=Lake_labels,
+                       clusters=Idents(BA9),
+                       assay.type.test = "logcounts",
+                       assay.type.ref = "logcounts")
 
 print('Plotting...')
 BA9$SingleR.pruned.calls <- BA9_SingleR$pruned.labels
 BA9$SingleR.calls <- BA9_SingleR$labels
-BA9$SingleR.cluster <- BA9_SingleR$cluster
+print(BA4.6_SingleR$cluster.labels)
 
-DimPlot(BA9, group.by="SingleR.cluster", label=TRUE, pt.size=0.5)
+new.cluster.ids <- BA4.6_SingleR$cluster.labels
+names(new.cluster.ids) <- levels(BA4.6)
+BA4.6 <- RenameIdents(BA4.6, new.cluster.ids)
+
+DimPlot(BA9, label=TRUE, pt.size=0.5)
 ggsave('/data/rusers/sheddn/UCLA-ASD/plots/UMAP_Harmony_BA9_integrated_SingleRlabel.png', width = 8, height = 7)
 
 saveRDS(BA9, '/data/rusers/sheddn/UCLA-ASD/data/combined_BA9_SingleR.RDS')
