377587b49e31b9803cdf0b461d6b8366871a6839
nshedd <58157630+nshedd@users.noreply.github.com>
2021-03-29 10:05:29 -0400

    Update UCLA-ASD_Integrated_proportions.R

--- UCLA-ASD_Integrated_proportions.R
+++ UCLA-ASD_Integrated_proportions.R
@@ -123,8 +123,8 @@ write.table(BA4.6_prop_df, '/data/rusers/sheddn/UCLA-ASD/data/CellTypeProportion
 
 BA9 <- readRDS('/data/rusers/sheddn/UCLA-ASD/data/combined_BA9_SingleR.RDS')
 
-new_ids = c("Ex1","Ex2","Oli1","Ast1","Ex3","Ex4","In1","In2","OPC1","Ex5","In3","In4","Ex6","Ex7","Ast2","Ex8","Ex9","Ex10","Ex11","Ex12",
-"Ex13","Oli2","Ex14","In5","Ast3","In6","Ast4","Ex15","In7","Ex16","Ex17","In8","OPC2","In8","Ex18","OPC3","In9","Ast5","OPC4")
+new_ids = c('Ex1'.'Ex2','Ex3','In1','Ast1','OPC1','Ex4','Mic','In2','Ex5','Ex6','In3','Ex7','Ex8','Ex9','Ast2','Oli1',
+            'Ex10','In4','Ex11','Ex12','Ex13','In5','Ex14','Ex15','In6','In7','Ex16','In8','Ex17')
 
 new.cluster.ids <- new_ids
 names(new.cluster.ids) <- levels(BA9)
