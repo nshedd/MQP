6414afd2da6030dbdbbeb672e9acc3cd767939e8
nshedd <58157630+nshedd@users.noreply.github.com>
2021-03-31 17:42:04 -0400

    Create UCLA-ASD_pulldata.R

---
+++ UCLA-ASD_pulldata.R
@@ -0,0 +1,16 @@
+library(Seurat)
+
+BA4.6 <- readRDS('/data/rusers/sheddn/UCLA-ASD/data/combined_BA4.6_WithDEGs.RDS')
+
+group = c('CTL','ASD')
+sample = c('BW11','BW12','BW13','BW15','BW2','BW23','BW25','BW30','BW35','BW46','BW5','BW6','BW8','BW1','BW10','BW14','BW16','BW17','BW22','BW24','BW26','BW27','BW3','BW31','BW32','BW4','BW59','BW60','BW7','BW9')
+cluster = range(0,39)
+
+for (c in Cluster){
+  for (s in Sample) {
+    data <- as.matrix(GetAssayData(BA4.6, slot = "counts")[, WhichCells(BA4.6, ident=c)])
+    data <- subset(data, subset = Sample==s)
+  }
+}
+
+head(data)
