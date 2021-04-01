d302f87e74cae01f9e2d26e3862d20f97d51032d
nshedd <58157630+nshedd@users.noreply.github.com>
2021-04-01 13:35:33 -0400

    Update UCLA-ASD_pulldata.R

--- UCLA-ASD_pulldata.R
+++ UCLA-ASD_pulldata.R
@@ -6,12 +6,9 @@ group = c('CTL','ASD')
 sample = unique(BA4.6$Sample)
 cluster = unique(Idents(BA4.6))
 
-for (c in cluster){
-  for (s in sample) {
-    data <- as.matrix(GetAssayData(BA4.6, slot = "counts"))
-    data <- subset(data, idents = c)
-    data <- subset(data, subset = "Sample"==s)
-    head(data)
-  }
+for (s in sample) {
+  data <- subset(BA4.6, subset = "Sample"==s)
+  data.expression = AverageExpression(data)
+  head(data.expression)
 }
 
