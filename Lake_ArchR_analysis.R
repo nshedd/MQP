1d4e5da6004a0c2735c9443271516d6c5c99b89e
nshedd <58157630+nshedd@users.noreply.github.com>
2021-04-05 13:30:28 -0400

    Update Lake_ArchR_analysis.R

--- Lake_ArchR_analysis.R
+++ Lake_ArchR_analysis.R
@@ -15,7 +15,7 @@ reformatFragmentFiles(
 
 ArrowFiles = createArrowFiles( inputFiles = fragment, sampleNames = key,
   minTSS = 4, minFrags = 1000, addTileMat = TRUE, addGeneScoreMat = TRUE,
-  gsubExpression=":.*", Force=TRUE)
+  gsubExpression=":.*", force=TRUE)
 
 proj = ArchRProject(ArrowFiles = ArrowFiles, outputDirectory = time, copyArrows = TRUE)
 print(proj$cellNames)
