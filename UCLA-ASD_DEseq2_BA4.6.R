e45a7542d39c165962c3d7ad651739916ab86052
nshedd <58157630+nshedd@users.noreply.github.com>
2021-04-05 22:28:53 -0400

    Update UCLA-ASD_DEseq2_BA4.6.R

--- UCLA-ASD_DEseq2_BA4.6.R
+++ UCLA-ASD_DEseq2_BA4.6.R
@@ -5,7 +5,30 @@ library(DESeq2)
 metaData = read.table("/data/rusers/sheddn/UCLA-ASD/data/DEG_output/BA4.6_metadata.txt", sep='\t', header=TRUE)
 head(metaData)
 
-for (i in range(0,38)) {
+countData = read.table(file="/data/rusers/sheddn/UCLA-ASD/data/DEG_output/BA4.6_averageexpression_cluster0.txt", sep="\t")
+head(countData)
+
+dds <- DESeqDataSetFromMatrix(countData=countData, 
+                              colData=metaData, 
+                              design=~Group, tidy = TRUE)
+dds <- DESeq(dds)
+
+res <- results(dds)
+head(results(dds, tidy=TRUE))
+
+#reset par
+par(mfrow=c(1,1))
+# Make a basic volcano plot
+with(res, plot(log2FoldChange, -log10(pvalue), pch=20, main="Volcano plot", xlim=c(-3,3)))
+
+# Add colored points: blue if padj<0.01, red if log2FC>1 and padj<0.05)
+with(subset(res, padj<.01 ), points(log2FoldChange, -log10(pvalue), pch=20, col="blue"))
+with(subset(res, padj<.01 & abs(log2FoldChange)>2), points(log2FoldChange, -log10(pvalue), pch=20, col="red"))
+
+ggsave("/data/rusers/sheddn/UCLA-ASD/plots/DEG_cluster0.png")
+
+q()
+for (i in range(1,38)) {
   file = paste("/data/rusers/sheddn/UCLA-ASD/data/DEG_output/BA4.6_averageexpression_cluster", i, ".txt", sep='')
   print(file)
   countData = read.table(file=file, sep="\t")
