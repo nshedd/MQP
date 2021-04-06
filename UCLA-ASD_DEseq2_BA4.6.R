f1aef1ba5134065e1fcf844bd612208fd12a7b8f
nshedd <58157630+nshedd@users.noreply.github.com>
2021-04-05 22:17:33 -0400

    Update UCLA-ASD_DEseq2_BA4.6.R

--- UCLA-ASD_DEseq2_BA4.6.R
+++ UCLA-ASD_DEseq2_BA4.6.R
@@ -7,7 +7,7 @@ head(metaData)
 
 for (i in range(0,38)) {
   file = paste("/data/rusers/sheddn/UCLA-ASD/data/DEG_output/BA4.6_averageexpression_cluster", i, ".txt", sep='')
-  countData = read.table(file, header=TRUE, row.names=1, sep="\t")
+  countData = read.table(file, header=TRUE, sep="\t")
   head(countData)
   dds <- DESeqDataSetFromMatrix(countData=countData, 
                                 colData=metaData, 
