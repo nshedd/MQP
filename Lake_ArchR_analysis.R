90d6a1f3c7ba78eb0de95e5053823abf54c3d406
nshedd <58157630+nshedd@users.noreply.github.com>
2021-04-11 23:16:15 -0400

    Update Lake_ArchR_analysis.R

--- Lake_ArchR_analysis.R
+++ Lake_ArchR_analysis.R
@@ -76,9 +76,17 @@ markerList <- getMarkers(markersGS, cutOff = "FDR <= 0.01 & Log2FC >= 1.25")
 
 write.table(markerList, file ="/data/rusers/sheddn/Lake_ths-seq/overexpressed_genes.txt", sep = '\t')
 
+marker_gene_table = read.table(path.expand("~/Zlab single-cell marker genes - Brain 4.tsv"), header=TRUE, sep="\t")
+all_known_marker_genes = marker_gene_table$Human.Gene
+
+intersection = intersect(markerList$name, all_known_marker_genes)
+
+markerGenes  <- intersection
+
 heatmapGS <- markerHeatmap(
   seMarker = markersGS, 
   cutOff = "FDR <= 0.01 & Log2FC >= 1.25",
+  labelMarkers = markerGenes,
   transpose = TRUE
 )
 
