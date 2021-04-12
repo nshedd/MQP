2f23f85768a9203cb24503366de7b8f921b29eec
nshedd <58157630+nshedd@users.noreply.github.com>
2021-04-12 13:37:58 -0400

    Update UCLA-ASD_DEG_Seurat.R

--- UCLA-ASD_DEG_Seurat.R
+++ UCLA-ASD_DEG_Seurat.R
@@ -5,8 +5,10 @@ library(cowplot)
 
 BA4.6 <- readRDS('/data/rusers/sheddn/UCLA-ASD/data/combined_BA4.6_SingleR.RDS')
 
+print(Idents(BA4.6))
+
 DefaultAssay(BA4.6) <- "RNA"
-markers <- FindConservedMarkers(BA4.6, ident.1="Ex1", grouping.var = "Group", verbose = FALSE)
+markers <- FindConservedMarkers(BA4.6, ident.1="0", grouping.var = "Group", verbose = FALSE)
 write.table(markers, '/data/rusers/sheddn/UCLA-ASD/data/BA4.6_DEGs_byGroup.txt')
 
 markers %>% group_by(cluster) %>% top_n(n = 3, wt = CTL_avg_logFC)
@@ -17,7 +19,7 @@ head(markers)
 BA9 <- readRDS('/data/rusers/sheddn/UCLA-ASD/data/combined_BA9_SingleR.RDS')
 
 DefaultAssay(BA9) <- "RNA"
-markers <- FindConservedMarkers(BA9, ident.1="Ex1", grouping.var = "Group", verbose = FALSE)
+markers <- FindConservedMarkers(BA9, ident.1="0", grouping.var = "Group", verbose = FALSE)
 write.table(markers, '/data/rusers/sheddn/UCLA-ASD/data/BA9_DEGs_byGroup.txt')
 
 markers %>% group_by(cluster) %>% top_n(n = 3, wt = CTL_avg_logFC)
