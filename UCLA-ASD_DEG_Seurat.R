30c5daa91da181bb7c5b57d53501d74ff216d49f
nshedd <58157630+nshedd@users.noreply.github.com>
2021-04-12 13:59:04 -0400

    Update UCLA-ASD_DEG_Seurat.R

--- UCLA-ASD_DEG_Seurat.R
+++ UCLA-ASD_DEG_Seurat.R
@@ -3,7 +3,13 @@ library(Seurat)
 library(ggplot2)
 library(cowplot)
 
-BA4.6 <- readRDS('/data/rusers/sheddn/UCLA-ASD/data/combined_BA4.6_SingleR.RDS')
+BA4.6 <- readRDS('/data/rusers/sheddn/UCLA-ASD/data/combined_BA4.6_WithDEGs.RDS')
+
+new_ids = c("Ex1","Ex2","Oli1","Ast1","Ex3","Ex4","In1","In2","OPC1","Ex5","In3","In4","Ex6","Ex7","Ast2","Ex8","Ex9","Ex10","Ex11","Ex12",
+            "Ex13","Oli2","Ex14","In5","Ast3","In6","Ast4","Ex15","In7","Ex16","Ex17","In8","OPC2","In8","Ex18","OPC3","In9","Ast5","OPC4")
+new.cluster.ids <- new_ids
+names(new.cluster.ids) <- levels(BA4.6)
+BA4.6 <- RenameIdents(BA4.6, new.cluster.ids)
 
 print(Idents(BA4.6))
 
