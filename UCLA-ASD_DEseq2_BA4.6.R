8cf31192106fe5e0e8af234c0a041285c1c9bb6e
nshedd <58157630+nshedd@users.noreply.github.com>
2021-04-16 10:37:50 -0400

    Update UCLA-ASD_DEseq2_BA4.6.R

--- UCLA-ASD_DEseq2_BA4.6.R
+++ UCLA-ASD_DEseq2_BA4.6.R
@@ -3,6 +3,9 @@ library(DESeq2)
 library(ggplot2)
 ## BA4/6
 
+
+## I manually extracted the cells from cluster 0 (an excitatory neuron cluster)
+## I could also take the entire dataset and include cluster in the design, but I thought it might be more interesting to do this way
 metaData = read.table("/data/rusers/sheddn/UCLA-ASD/data/DEG_output/BA4.6_metadata_cluster0.txt", sep='\t', header=TRUE)
 head(metaData)
 
@@ -10,9 +13,12 @@ countData = read.table(file="/data/rusers/sheddn/UCLA-ASD/data/DEG_output/BA4.6_
 head(countData)
 countData = countData + 4
 
+# Group = CTL or ASD
+# Sample = Donor
+
 dds <- DESeqDataSetFromMatrix(countData=countData, 
                               colData=metaData, 
-                              design=~Group)
+                              design=~Group) ## ideally this would be ~Group + Sample  or ~Group + Sample + Cluster, but it wouldn't let me do Group+Sample
 dds <- DESeq(dds)
 
 res <- results(dds)
@@ -30,6 +36,7 @@ with(subset(res, padj<.01 & abs(log2FoldChange)>2), points(log2FoldChange, -log1
 ggsave("/data/rusers/sheddn/UCLA-ASD/plots/DEG_cluster0.png")
 
 q()
+
 for (i in range(1,38)) {
   metadata_file = paste("/data/rusers/sheddn/UCLA-ASD/data/DEG_output/BA4.6_metadata_cluster", i, ".txt", sep='')
   countData = read.table(file=metadata_file, sep="\t")
