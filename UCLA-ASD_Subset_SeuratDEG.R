ce63261c6aff4664a42b214ce4a23ae1da063bfd
nshedd <58157630+nshedd@users.noreply.github.com>
2021-04-17 19:51:13 -0400

    Create UCLA-ASD_Subset_SeuratDEG.R

---
+++ UCLA-ASD_Subset_SeuratDEG.R
@@ -0,0 +1,27 @@
+library(Seurat)
+library(cowplot)
+
+BA4.6 <- readRDS('/data/rusers/sheddn/UCLA-ASD/subset/Seurat_integrated/data/BA4.6_WithDEGs.RDS')
+
+clusters_BA4.6 <- c('Ex1','Ex2','Ex3','In1','Ex4','OPC','In2','Oli','Ex5','Ex6',
+                    'In3','Ast1','In4','Ex7','Ex8','Ex9','In5','Ex10','Ex11','Ast2',
+                    'Ex12','Ex13','In6','In7','In8','Ex14','Ex15','Ex16')
+
+new.cluster.ids <- clusters_BA4.6
+names(new.cluster.ids) <- levels(BA4.6)
+BA4.6 <- RenameIdents(BA4.6, new.cluster.ids)
+
+for (i in clusters_BA4.6) {
+  print(i)
+  
+  sub <- subset(BA4.6, idents = i)
+  Idents(sub) <- "Group"
+  avg.sub <- log1p(AverageExpression(sub, verbose = FALSE)$RNA)
+  avg.sub$gene <- rownames(avg.sub)
+  
+  p1 <- ggplot(avg.t.cells, aes(ASD, CTL)) + geom_point() + ggtitle(i)
+  p1 <- LabelPoints(plot = p1, points = genes.to.label, repel = TRUE)
+  
+  link = paste("/data/rusers/sheddn/UCLA-ASD/subset/Seurat_integrated/plots/BA4.6_DEG_",i,'.png')
+  ggsave(link)
+}
