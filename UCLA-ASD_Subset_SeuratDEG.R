ee2495ad0d49562f9510ab668429b7ca19944ff9
nshedd <58157630+nshedd@users.noreply.github.com>
2021-04-17 20:06:37 -0400

    Update UCLA-ASD_Subset_SeuratDEG.R

--- UCLA-ASD_Subset_SeuratDEG.R
+++ UCLA-ASD_Subset_SeuratDEG.R
@@ -19,7 +19,7 @@ for (i in clusters_BA4.6) {
   avg.sub$gene <- rownames(avg.sub)
   
   Idents(BA4.6) <- "celltype.stim"
-  interferon.response <- FindMarkers(BA4.6, ident.1 = paste(i, "ASD", sep="_"), ident.2 = paste(i, "CTL", sep="_"), verbose = FALSE)
+  interferon.response <- FindMarkers(BA4.6, ident.1 = paste(i, "ASD", sep="."), ident.2 = paste(i, "CTL", sep="."), verbose = FALSE)
   
   genes.to.label <- row.names(interferon.response[1:10,])
   
