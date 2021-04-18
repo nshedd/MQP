ec676d374d572c3d381cef2101594b4b89de8381
nshedd <58157630+nshedd@users.noreply.github.com>
2021-04-18 12:47:00 -0400

    Update UCLA-ASD_Subset_SeuratDEG.R

--- UCLA-ASD_Subset_SeuratDEG.R
+++ UCLA-ASD_Subset_SeuratDEG.R
@@ -15,7 +15,7 @@ BA4.6 <- RenameIdents(BA4.6, new.cluster.ids)
 clusters_BA4.6 <- c('Ex1','Ex3','In1','OPC','In2','Oli','Ex5','Ex6',
                     'In3','Ast1','In4','Ex7','Ex8','Ex9','In5','Ex10','Ex11','Ast2',
                     'Ex12','Ex13','In6','In7','In8','Ex14','Ex15','Ex16')
-                    #No DEGs pass threshold - 'Ex2','Ex4',
+                    #No DEGs pass threshold - 'Ex2','Ex4','Ex16'
 
 BA4.6_full <- BA4.6
 
@@ -34,9 +34,11 @@ for (i in clusters_BA4.6) {
   Idents(BA4.6_full) <- "celltype.Group"
   interferon.response <- FindMarkers(BA4.6_full, ident.1 = paste(i, "ASD", sep="_"), ident.2 = paste(i, "CTL", sep="_"), verbose = FALSE)
   
+  head(interferon.response, n=10)
+  
   genes.to.label <- row.names(interferon.response[1:10,])
   
-  p1 <- ggplot(avg.sub, aes(ASD, CTL)) + geom_point() + ggtitle(i)
+  p1 <- ggplot(avg.sub, aes(CTL, ASD)) + geom_point() + ggtitle(i)
   p1 <- LabelPoints(plot = p1, points = genes.to.label, repel = TRUE)
   
   link = paste("/data/rusers/sheddn/UCLA-ASD/subset/Seurat_integrated/plots/BA4.6_DEGS_",i,'.png', sep='')
