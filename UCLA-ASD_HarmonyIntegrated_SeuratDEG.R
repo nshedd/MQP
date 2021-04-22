9e0b2f78e080b58cc1229c44cfb77101935ece6f
nshedd <58157630+nshedd@users.noreply.github.com>
2021-04-22 11:22:04 -0400

    Update UCLA-ASD_HarmonyIntegrated_SeuratDEG.R

--- UCLA-ASD_HarmonyIntegrated_SeuratDEG.R
+++ UCLA-ASD_HarmonyIntegrated_SeuratDEG.R
@@ -3,46 +3,6 @@ library(Seurat)
 
 BA9 <- readRDS('/data/rusers/sheddn/UCLA-ASD/data/combined_BA9_DoubletsRemoved')
 
-clusters_BA9 = c("Ex1","Ex2","Ex3","In1","Ast1","OPC1","Mic","Ex4","In2","Ex5",
-                 "In3","Ex6","Ex7","In4","Ex8","Oli","Ast2","Ex9","Ex10","Ex11",
-                 "Ex11","Ex12","Ex13","Ex14","Ast3","In5","In6","Ex15","Ex16","In7",
-                 "OPC2","Ex17")
-new.cluster.ids <- clusters_BA9
-names(new.cluster.ids) <- levels(BA9)
-BA9 <- RenameIdents(BA9, new.cluster.ids)
-
-clusters_BA9 = c("Ex1","Ex2","Ex3","In1","Ast1","OPC1","Mic","Ex4","In2","Ex5",
-                 "In3","Ex6","Ex7","In4","Ex8","Oli","Ast2","Ex9","Ex10","Ex11",
-                 "Ex11","Ex12","Ex13","Ex14","Ast3","In5","In6","Ex15","Ex16","In7",
-                 "OPC2","Ex17")
-
-BA9_full <- BA9
-
-for (i in clusters_BA9) {
-  BA9_full <- BA9
-  
-  print(i)
-  
-  sub <- subset(BA9_full, idents = i)
-  Idents(sub) <- "Group"
-  avg.sub <- log1p(AverageExpression(sub, verbose = FALSE)$RNA)
-  avg.sub$gene <- rownames(avg.sub)
-  
-  print(avg.sub[1:25,])
-  
-  avg.sub$diff = abs(avg.sub$ASD - avg.sub$CTL)
-  avg.sub <- avg.sub[order(avg.sub$diff, decreasing=TRUE),] 
-  
-  genes.to.label <- row.names(avg.sub[1:25,])
-  
-  p1 <- ggplot(avg.sub, aes(CTL, ASD)) + geom_point() + ggtitle(i)
-  p1 <- LabelPoints(plot = p1, points = genes.to.label, repel = TRUE)
-  
-  link = paste("/data/rusers/sheddn/UCLA-ASD/plots/DEG-nomito/BA9_DEGS_",i,'.png', sep='')
-  ggsave(link)
-}
-
-
 clusters_BA9 = c("Ex","Ex","Ex","In","Ast","OPC","Mic","Ex","In","Ex",
                  "In","Ex","Ex","In","Ex","Oli","Ast","Ex","Ex","Ex",
                  "Ex","Ex","Ex","Ex","Ast","In","In","Ex","Ex","In",
@@ -66,12 +26,21 @@ for (i in clusters_BA9) {
   avg.sub <- log1p(AverageExpression(sub, verbose = FALSE)$RNA)
   avg.sub$gene <- rownames(avg.sub)
   
-  print(avg.sub[1:10,])
+  avg.sub$celltype.group <- paste(Idents(avg.sub,avg.sub$Group, sep = "_"))
+  avg.sub$celltype <- Idents(avg.sub)
+  Idents(avg.sub) <- "celltype.group"
+  response <- FindMarkers(avg.sub, ident.1 = paste(i,"ASD", sep='_'), ident.2 = paste(i,"CTL", sep='_'), verbose = FALSE)
+  print(response[1:25,])
+  
+  link = paste("/data/rusers/sheddn/UCLA-ASD/plots/DEG-nomito/BA4.6_DEGs-FDR_",i,'.txt', sep='')
+  write.table(response, link)
+  
+  genes.to.label <- row.names(response[1:25,])
   
   avg.sub$diff = abs(avg.sub$ASD - avg.sub$CTL)
   avg.sub <- avg.sub[order(avg.sub$diff, decreasing=TRUE),] 
   
-  genes.to.label <- row.names(avg.sub[1:10,])
+  genes.to.label <- row.names(avg.sub[1:25,])
   
   p1 <- ggplot(avg.sub, aes(CTL, ASD)) + geom_point() + ggtitle(i)
   p1 <- LabelPoints(plot = p1, points = genes.to.label, repel = TRUE)
@@ -79,9 +48,6 @@ for (i in clusters_BA9) {
   link = paste("/data/rusers/sheddn/UCLA-ASD/plots/DEG-nomito/BA9_DEGS_",i,'.png', sep='')
   ggsave(link)
 }
-
-q()
-
 
 BA4.6 <- readRDS('/data/rusers/sheddn/UCLA-ASD/data/combined_BA4.6_WithDEGs.RDS')
 
@@ -109,12 +75,21 @@ for (i in clusters_BA4.6) {
   avg.sub <- log1p(AverageExpression(sub, verbose = FALSE)$RNA)
   avg.sub$gene <- rownames(avg.sub)
   
+  print(avg.sub[1:25,])
+  
   avg.sub$diff = abs(avg.sub$ASD - avg.sub$CTL)
   avg.sub <- avg.sub[order(avg.sub$diff, decreasing=TRUE),] 
   
-  print(avg.sub[1:25,])
+  avg.sub$celltype.group <- paste(Idents(avg.sub,avg.sub$Group, sep = "_"))
+  avg.sub$celltype <- Idents(avg.sub)
+  Idents(avg.sub) <- "celltype.group"
+  response <- FindMarkers(avg.sub, ident.1 = paste(i,"ASD", sep='_'), ident.2 = paste(i,"CTL", sep='_'), verbose = FALSE)
+  print(response[1:25,])
   
-  genes.to.label <- row.names(avg.sub[1:25,])
+  link = paste("/data/rusers/sheddn/UCLA-ASD/plots/DEG-nomito/BA4.6_DEGs-FDR_",i,'.txt', sep='')
+  write.table(response, link)
+  
+  genes.to.label <- row.names(response[1:25,])
   
   p1 <- ggplot(avg.sub, aes(CTL, ASD)) + geom_point() + ggtitle(i)
   p1 <- LabelPoints(plot = p1, points = genes.to.label, repel = TRUE)
@@ -123,7 +98,9 @@ for (i in clusters_BA4.6) {
   ggsave(link)
 }
 
+
 q()
+
 
 clusters_BA4.6 = c("Ex1","Ex2","Ex3","In1","Ast1","Oli1","In2","OPC1","In3","Ex4",
                    "Ex5","Ex6","Ex7","Ex8","Ex9","In4","Ex10","Ex11","In5","Ex12",
@@ -140,7 +117,7 @@ clusters_BA4.6 = c("Ex1","Ex2","Ex3","In1","Ast1","Oli1","In2","OPC1","In3","Ex4
                    "Ex5","Ex6","Ex7","Ex8","Ex9","In4","Ex10","Ex11","In5","Ex12",
                    "Ex13","Oli2","Ex14","Ast2","Ast3","In6","Ast4","In7","Mic1","In8",
                    "Oli3","In9","Ex15","In10","In11","OPC2","In12","Ex16","Ast5","In13")
-                   # only in 1 group - "Ast6", "OPC3", "Mic2"
+# only in 1 group - "Ast6", "OPC3", "Mic2"
 
 BA4.6_full <- BA4.6
 
@@ -168,26 +145,27 @@ for (i in clusters_BA4.6) {
   ggsave(link)
 }
 
-clusters_BA4.6 = c("Ex","Ex","Ex","In","Ast","Oli","In","OPC","In","Ex",
-                   "Ex","Ex","Ex","Ex","Ex","In","Ex","Ex","In","Ex",
-                   "Ex","Oli","Ex","Ast","Ast","In","Ast","In","Mic","In",
-                   "Oli","In","Ex","In","In","OPC","In","Ex","Ast","In",
-                   "Ast","OPC","Mic")
+clusters_BA9 = c("Ex1","Ex2","Ex3","In1","Ast1","OPC1","Mic","Ex4","In2","Ex5",
+                 "In3","Ex6","Ex7","In4","Ex8","Oli","Ast2","Ex9","Ex10","Ex11",
+                 "Ex11","Ex12","Ex13","Ex14","Ast3","In5","In6","Ex15","Ex16","In7",
+                 "OPC2","Ex17")
+new.cluster.ids <- clusters_BA9
+names(new.cluster.ids) <- levels(BA9)
+BA9 <- RenameIdents(BA9, new.cluster.ids)
 
-new.cluster.ids <- clusters_BA4.6
-names(new.cluster.ids) <- levels(BA4.6)
-BA4.6 <- RenameIdents(BA4.6, new.cluster.ids)
+clusters_BA9 = c("Ex1","Ex2","Ex3","In1","Ast1","OPC1","Mic","Ex4","In2","Ex5",
+                 "In3","Ex6","Ex7","In4","Ex8","Oli","Ast2","Ex9","Ex10","Ex11",
+                 "Ex11","Ex12","Ex13","Ex14","Ast3","In5","In6","Ex15","Ex16","In7",
+                 "OPC2","Ex17")
 
-clusters_BA4.6 = c('Ast','Ex','In','Mic','OPC','Oli')
+BA9_full <- BA9
 
-BA4.6_full <- BA4.6
-
-for (i in clusters_BA4.6) {
-  BA4.6_full <- BA4.6
+for (i in clusters_BA9) {
+  BA9_full <- BA9
   
   print(i)
   
-  sub <- subset(BA4.6_full, idents = i)
+  sub <- subset(BA9_full, idents = i)
   Idents(sub) <- "Group"
   avg.sub <- log1p(AverageExpression(sub, verbose = FALSE)$RNA)
   avg.sub$gene <- rownames(avg.sub)
@@ -202,9 +180,7 @@ for (i in clusters_BA4.6) {
   p1 <- ggplot(avg.sub, aes(CTL, ASD)) + geom_point() + ggtitle(i)
   p1 <- LabelPoints(plot = p1, points = genes.to.label, repel = TRUE)
   
-  link = paste("/data/rusers/sheddn/UCLA-ASD/plots/DEG-nomito/BA4.6_DEGS_",i,'.png', sep='')
+  link = paste("/data/rusers/sheddn/UCLA-ASD/plots/DEG-nomito/BA9_DEGS_",i,'.png', sep='')
   ggsave(link)
 }
-
-q()
 
