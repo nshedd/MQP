3866e65cd981fa54706e082979ce233616e6389b
nshedd <58157630+nshedd@users.noreply.github.com>
2021-04-22 09:33:31 -0400

    Update UCLA-ASD_integrated_RunSingleR.R

--- UCLA-ASD_integrated_RunSingleR.R
+++ UCLA-ASD_integrated_RunSingleR.R
@@ -20,26 +20,26 @@ Lake_SCE <- as.SingleCellExperiment(Lake)
 Lake_labels <- Idents(Lake)
 
 
-BA4.6 <- readRDS('/data/rusers/sheddn/UCLA-ASD/data/combined_BA4.6_WithDEGs.RDS')
+BA9 <- readRDS('/data/rusers/sheddn/UCLA-ASD/data/combined_BA9_WithDEGs.RDS')
 
 
 print('Running SingleR...')
-BA4.6_SingleR <- SingleR(test=GetAssayData(BA4.6, assay = "RNA"),
+BA9_SingleR <- SingleR(test=GetAssayData(BA9, assay = "RNA"),
                          ref=Lake_SCE,
                          method='cluster',
                          labels=Lake_labels,
-                         clusters=Idents(BA4.6),
+                         clusters=Idents(BA9),
                          assay.type.test = "logcounts",
                          assay.type.ref = "logcounts")
 
-print(BA4.6_SingleR$labels)
-write.table(BA4.6_SingleR$labels, "/data/rusers/sheddn/UCLA-ASD/data/BA4.6_cluster_ids.txt")
+print(BA9_SingleR$labels)
+write.table(BA9_SingleR$labels, "/data/rusers/sheddn/UCLA-ASD/data/BA9_cluster_ids.txt")
 
-new.cluster.ids <- BA4.6_SingleR$labels
-names(new.cluster.ids) <- levels(BA4.6)
-BA4.6 <- RenameIdents(BA4.6, new.cluster.ids)
+new.cluster.ids <- BA9_SingleR$labels
+names(new.cluster.ids) <- levels(BA9)
+BA9 <- RenameIdents(BA9, new.cluster.ids)
 
 print('Plotting...')
 
-DimPlot(BA4.6, label=TRUE, pt.size=0.5)
-ggsave('/data/rusers/sheddn/UCLA-ASD/plots/UMAP_Harmony_BA4.6_integrated_SingleRlabel.png', width = 8, height = 7)
+DimPlot(BA9, label=TRUE, pt.size=0.5)
+ggsave('/data/rusers/sheddn/UCLA-ASD/plots/UMAP_Harmony_BA9_integrated_SingleRlabel.png', width = 8, height = 7)
