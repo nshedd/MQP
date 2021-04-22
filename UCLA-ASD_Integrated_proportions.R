62daca4077a78d4b6ed14ef2bf79bb14d38d7c59
nshedd <58157630+nshedd@users.noreply.github.com>
2021-04-22 09:32:53 -0400

    Update UCLA-ASD_Integrated_proportions.R

--- UCLA-ASD_Integrated_proportions.R
+++ UCLA-ASD_Integrated_proportions.R
@@ -3,6 +3,94 @@ library(Seurat)
 library(ggplot2)
 
 
+## Read and relabel clusters and plot
+BA9 <- readRDS('/data/rusers/sheddn/UCLA-ASD/data/combined_BA9_WithDEGs.RDS')
+
+p1 <- DimPlot(BA9, reduction = "umap", group.by = "Group")
+p2 <- DimPlot(BA9, reduction = "umap", label = TRUE)
+p3 <- p1 + p2
+ggsave('/data/rusers/sheddn/UCLA-ASD/plots/UMAP_Harmony_BA9_integrated_combined.png', width = 16, height = 7)
+
+new_ids = c('Ex1','Ex2','Ex3','In1','Ast1','OPC1','Ex4','Mic','In2','Ex5',
+            'Ex6','In3','Ex7','Ex8','Ex9','Ast2','Oli1','Ex10','In4','Ex11',
+            'Ex12','Ex13','In5','Ex14','Ex15','Ex16','In6','In7','Ex17','In8','Ex18')
+
+new.cluster.ids <- new_ids
+names(new.cluster.ids) <- levels(BA9)
+BA9 <- RenameIdents(BA9, new.cluster.ids)
+
+DimPlot(BA9, label=TRUE, pt.size=0.5)
+ggsave('/data/rusers/sheddn/UCLA-ASD/plots/UMAP_Harmony_BA9_integrated_SingleRlabel_bycluster_relabeled.png', width = 8, height = 7)
+
+DimPlot(BA9, reduction = "umap", label=TRUE, split.by = "Group")
+ggsave('/data/rusers/sheddn/UCLA-ASD/plots/UMAP_Harmony_BA9_integrated_SingleRlabel_bycluster_separate_relabeled.png', width = 16, height = 7)
+
+## By cluster proportion analysis
+ASD_BA9 <- subset(BA9, subset = Group == "ASD")
+
+ASD_BA9_prop_manual_list = Idents(ASD_BA9)
+ASD_BA9_prop_manual_table = table(ASD_BA9_prop_manual_list)
+
+ASD_BA9_prop_df = ASD_BA9_prop_manual_table %>% as.data.frame
+colnames(ASD_BA9_prop_df) <- c('Cell_Type', 'FreqASD')
+
+ASD_BA9_sum = sum(ASD_BA9_prop_df$FreqASD)
+ASD_BA9_prop_df$FreqASD = ASD_BA9_prop_df$FreqASD/ASD_BA9_sum
+
+for (i in unique(ASD_BA9$Sample)) {
+  data <- subset(ASD_BA9, subset = Sample == i)
+  
+  lab = paste("ASD", i)
+  
+  ASD_BA9_prop_manual_list = Idents(data)
+  ASD_BA9_prop_manual_table = table(ASD_BA9_prop_manual_list)
+  
+  df = ASD_BA9_prop_manual_table %>% as.data.frame
+  colnames(df) <- c('Cell_Type', lab)
+  
+  ASD_BA9_sum = sum(df[lab])
+  df[lab] = df[lab]/ASD_BA9_sum
+  
+  ASD_BA9_prop_df = merge(x = ASD_BA9_prop_df, y = df, by = 'Cell_Type', all = TRUE)
+}
+
+
+CTL_BA9 <- subset(BA9, subset = Group == "CTL")
+
+CTL_BA9_prop_manual_list = Idents(CTL_BA9)
+CTL_BA9_prop_manual_table = table(CTL_BA9_prop_manual_list)
+
+CTL_BA9_prop_df = CTL_BA9_prop_manual_table %>% as.data.frame
+colnames(CTL_BA9_prop_df) <- c('Cell_Type', 'FreqCTL')
+
+CTL_BA9_sum = sum(CTL_BA9_prop_df$FreqCTL)
+CTL_BA9_prop_df$FreqCTL = CTL_BA9_prop_df$FreqCTL/CTL_BA9_sum
+
+for (i in unique(CTL_BA9$Sample)) {
+  data <- subset(CTL_BA9, subset = Sample == i)
+  
+  lab = paste("CTL", i)
+  
+  CTL_BA9_prop_manual_list = Idents(data)
+  CTL_BA9_prop_manual_table = table(CTL_BA9_prop_manual_list)
+  
+  df = CTL_BA9_prop_manual_table %>% as.data.frame
+  colnames(df) <- c('Cell_Type', lab)
+  
+  CTL_BA9_sum = sum(df[lab])
+  df[lab] = df[lab]/CTL_BA9_sum
+  
+  CTL_BA9_prop_df = merge(x = CTL_BA9_prop_df, y = df, by = 'Cell_Type', all = TRUE)
+}
+
+BA9_prop_df = merge(x = ASD_BA9_prop_df, y = CTL_BA9_prop_df, by = 'Cell_Type', all = TRUE)
+print(BA9_prop_df)
+
+write.table(BA9_prop_df, '/data/rusers/sheddn/UCLA-ASD/data/CellTypeProportions_SingleRbyclust_BA9.txt', sep=',')
+
+q()
+
+
 BA4.6 <- readRDS('/data/rusers/sheddn/UCLA-ASD/data/combined_BA4.6_WithDEGs.RDS')
 
 new_ids = c("Ex1","Ex2","Ex3","In1","Ast1","Oli1","In2","OPC1","In3","Ex4",
@@ -88,82 +176,3 @@ write.table(BA4.6_prop_df, '/data/rusers/sheddn/UCLA-ASD/data/CellTypeProportion
 
 q()
 
-## Read and relabel clusters and plot
-BA9 <- readRDS('/data/rusers/sheddn/UCLA-ASD/data/combined_BA9_DoubletsRemoved')
-
-new_ids = c('Ex1','Ex2','Ex3','In1','Ast1','OPC1','Ex4','Mic','In2','Ex5',
-            'Ex6','In3','Ex7','Ex8','Ex9','Ast2','Oli1','Ex10','In4','Ex11',
-            'Ex12','Ex13','In5','Ex14','Ex15','Ex16','In6','In7','Ex17','In8','Ex18')
-
-new.cluster.ids <- new_ids
-names(new.cluster.ids) <- levels(BA9)
-BA9 <- RenameIdents(BA9, new.cluster.ids)
-
-DimPlot(BA9, label=TRUE, pt.size=0.5)
-ggsave('/data/rusers/sheddn/UCLA-ASD/plots/UMAP_Harmony_BA9_integrated_SingleRlabel_bycluster_relabeled.png', width = 8, height = 7)
-
-DimPlot(BA9, reduction = "umap", label=TRUE, split.by = "Group")
-ggsave('/data/rusers/sheddn/UCLA-ASD/plots/UMAP_Harmony_BA9_integrated_SingleRlabel_bycluster_separate_relabeled.png', width = 16, height = 7)
-
-## By cluster proportion analysis
-ASD_BA9 <- subset(BA9, subset = Group == "ASD")
-
-ASD_BA9_prop_manual_list = Idents(ASD_BA9)
-ASD_BA9_prop_manual_table = table(ASD_BA9_prop_manual_list)
-
-ASD_BA9_prop_df = ASD_BA9_prop_manual_table %>% as.data.frame
-colnames(ASD_BA9_prop_df) <- c('Cell_Type', 'FreqASD')
-
-ASD_BA9_sum = sum(ASD_BA9_prop_df$FreqASD)
-ASD_BA9_prop_df$FreqASD = ASD_BA9_prop_df$FreqASD/ASD_BA9_sum
-
-for (i in unique(ASD_BA9$Sample)) {
-  data <- subset(ASD_BA9, subset = Sample == i)
-  
-  lab = paste("ASD", i)
-  
-  ASD_BA9_prop_manual_list = Idents(data)
-  ASD_BA9_prop_manual_table = table(ASD_BA9_prop_manual_list)
-  
-  df = ASD_BA9_prop_manual_table %>% as.data.frame
-  colnames(df) <- c('Cell_Type', lab)
-  
-  ASD_BA9_sum = sum(df[lab])
-  df[lab] = df[lab]/ASD_BA9_sum
-  
-  ASD_BA9_prop_df = merge(x = ASD_BA9_prop_df, y = df, by = 'Cell_Type', all = TRUE)
-}
-
-
-CTL_BA9 <- subset(BA9, subset = Group == "CTL")
-
-CTL_BA9_prop_manual_list = Idents(CTL_BA9)
-CTL_BA9_prop_manual_table = table(CTL_BA9_prop_manual_list)
-
-CTL_BA9_prop_df = CTL_BA9_prop_manual_table %>% as.data.frame
-colnames(CTL_BA9_prop_df) <- c('Cell_Type', 'FreqCTL')
-
-CTL_BA9_sum = sum(CTL_BA9_prop_df$FreqCTL)
-CTL_BA9_prop_df$FreqCTL = CTL_BA9_prop_df$FreqCTL/CTL_BA9_sum
-
-for (i in unique(CTL_BA9$Sample)) {
-  data <- subset(CTL_BA9, subset = Sample == i)
-  
-  lab = paste("CTL", i)
-  
-  CTL_BA9_prop_manual_list = Idents(data)
-  CTL_BA9_prop_manual_table = table(CTL_BA9_prop_manual_list)
-  
-  df = CTL_BA9_prop_manual_table %>% as.data.frame
-  colnames(df) <- c('Cell_Type', lab)
-  
-  CTL_BA9_sum = sum(df[lab])
-  df[lab] = df[lab]/CTL_BA9_sum
-  
-  CTL_BA9_prop_df = merge(x = CTL_BA9_prop_df, y = df, by = 'Cell_Type', all = TRUE)
-}
-
-BA9_prop_df = merge(x = ASD_BA9_prop_df, y = CTL_BA9_prop_df, by = 'Cell_Type', all = TRUE)
-print(BA9_prop_df)
-
-write.table(BA9_prop_df, '/data/rusers/sheddn/UCLA-ASD/data/CellTypeProportions_SingleRbyclust_BA9.txt', sep=',')
